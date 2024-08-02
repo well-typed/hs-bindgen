7399d370212c26511a55dda403f3a8d3feb8d173
Edsko de Vries <edsko@well-typed.com>
2024-08-02 14:47:52 +0200

    Discuss generating funptrs from Haskell fns

--- alternatives/experiment/manual/app/Main.hs
+++ alternatives/experiment/manual/app/Main.hs
@@ -5,17 +5,34 @@ module Main (main) where
 import Foreign
 
 import HsBindgenCExample
+import System.IO
 
 main :: IO ()
 main = do
+    hSetBuffering stdout NoBuffering
+
+    -- Simplest example: call C function with no arguments, no result
     cHelloWorld
+
+    -- C function with a single argument
     cShowInt 1234
+
+    -- C function taking a pointer to a struct as argument
     alloca $ \ptr -> do
       poke ptr $ HaskellStruct 1234 5678
       cShowStruct ptr
+
+    -- C function taking a pointer to a function, passing another C function
     cCallFunPtr addrOf_cShowInt
-    funPtr <- cReturnFunPtr
-    callFunPtr_Void_Int funPtr 12345678
+
+    -- C function returning a pointer to a function, executing it in Haskell
+    funPtrFromC <- cReturnFunPtr
+    callFunPtr_Void_Int funPtrFromC 12345678
+
+    -- C function taking a pointer to a function, passing a Haskell function
+    funPtrFromHaskell <- wrapFunPtr_Void_Int $ \x ->
+      putStrLn $ "fromHaskell: " ++ show x
+    cCallFunPtr funPtrFromHaskell
 
 #ifdef INCLUDE_INVALID
     invalid_cHelloWorld_extraParam 0
