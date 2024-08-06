0b3e23387a14bc15b21f51b3bbd10efbb746b30c
Edsko de Vries <edsko@well-typed.com>
2024-08-06 12:46:14 +0200

    Link to `libclang`

---
+++ hs-bindgen/src/HsBindgen/Clang.hs
@@ -0,0 +1,10 @@
+-- | Bindings to @libclang@
+--
+-- The goal of this module is not to be a complete set of bindings for all of
+-- @libclang@, but rather only to the parts that we need.
+--
+-- Intended for qualified import.
+--
+-- > import Hsbindgen.Clang qualified as Clang
+module HsBindgen.Clang (
+  ) where
