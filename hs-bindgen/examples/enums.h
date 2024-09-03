9f2aba9687d5f0cb741cb36c02fc24846c065ad3
Oleg Grenrus <oleg.grenrus@iki.fi>
2024-09-03 14:11:34 +0300

    Add enum values

--- hs-bindgen/examples/enums.h
+++ hs-bindgen/examples/enums.h
@@ -6,4 +6,19 @@ enum first {
     FIRST2
 };
 
+enum second {
+    SECOND_A = -1,
+    SECOND_B,
+    SECOND_C
+};
+
+enum same {
+    SAME_A = 1,
+    SAME_B = 1,
+};
+
+enum packad {
+    PACKED_A, PACKED_B, PACKED_C
+} __attribute__((packed));
+
 #endif /* ENUMS_H */
