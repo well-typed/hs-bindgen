bab0380467747bc8379cca97c48048e2a59b4306
Travis Cardwell <travis.cardwell@extrema.is>
2024-10-10 10:33:26 +0900

    Finish dumpComment (#26)

--- clang-ast-dump/Main.hs
+++ clang-ast-dump/Main.hs
@@ -93,13 +93,14 @@ foldDecls opts@Options{..} parent cursor = do
         dumpComment 2 Nothing =<< clang_Cursor_getParsedComment cursor
 
     let doRecurse  = pure $ Recurse (foldDecls opts) (pure . const Nothing)
-        doContinue = Continue Nothing <$ traceL 1 "CONTINUE"
+        --doContinue = pure $ Continue Nothing
     case fromSimpleEnum cursorKind of
       Right CXCursor_StructDecl      -> doRecurse
       Right CXCursor_EnumDecl        -> doRecurse
       Right CXCursor_TypedefDecl     -> doRecurse
       Right CXCursor_MacroDefinition -> doRecurse
-      _otherwise                     -> doContinue
+      Right{} -> Continue Nothing <$ traceL 1 "CURSOR_KIND_NOT_IMPLEMENTED"
+      Left n  -> Continue Nothing <$ traceU 1 "CURSOR_KIND_ENUM_OUT_OF_RANGE" n
 
 dumpComment :: Int -> Maybe CUInt -> CXComment -> IO ()
 dumpComment level mIdx comment = do
@@ -107,30 +108,95 @@ dumpComment level mIdx comment = do
     maybe (traceU level) (traceO level) mIdx "kind" $ fromSimpleEnum commentKind
     traceWhen level1 "whitespace" =<< clang_Comment_isWhitespace comment
 
-    when (fromSimpleEnum commentKind == Right CXComment_Text) $
-      traceU level1 "text" =<< clang_TextComment_getText comment
+    case fromSimpleEnum commentKind of
+      Right CXComment_Null -> pure ()
 
-    when (fromSimpleEnum commentKind == Right CXComment_InlineCommand) $ do
-      traceU level1 "name" =<< clang_InlineCommandComment_getCommandName comment
-      traceU level1 "render kind" . fromSimpleEnum
-        =<< clang_InlineCommandComment_getRenderKind comment
-      argsCount <- clang_InlineCommandComment_getNumArgs comment
-      when (argsCount > 0) $ do
-        traceU level1 "args" argsCount
-        forM_ [0 .. argsCount - 1] $ \i ->
-          traceO_ level2 i =<< clang_InlineCommandComment_getArgText comment i
+      Right CXComment_Text ->
+        traceU level1 "text" =<< clang_TextComment_getText comment
 
-    -- TODO other kinds
+      Right CXComment_InlineCommand -> do
+        traceU level1 "name"
+          =<< clang_InlineCommandComment_getCommandName comment
+        traceU level1 "render kind" . fromSimpleEnum
+          =<< clang_InlineCommandComment_getRenderKind comment
+        numArgs <- clang_InlineCommandComment_getNumArgs comment
+        when (numArgs > 0) $ do
+          traceU level1 "args" numArgs
+          forM_ [0 .. numArgs - 1] $ \i ->
+            traceO_ level2 i =<< clang_InlineCommandComment_getArgText comment i
 
-    childCount <- clang_Comment_getNumChildren comment
-    when (childCount > 0) $ do
-      traceU level1 "children" childCount
-      forM_ [0 .. childCount - 1] $ \i ->
+      Right CXComment_HTMLStartTag -> do
+        traceU level1 "name" =<< clang_HTMLTagComment_getTagName comment
+        traceWhen level1 "self-closing"
+          =<< clang_HTMLStartTagComment_isSelfClosing comment
+        numAttrs <- clang_HTMLStartTag_getNumAttrs comment
+        when (numAttrs > 0) $ do
+          traceU level1 "attributes" numAttrs
+          forM_ [0 .. numAttrs - 1] $ \i -> do
+            traceO_ level2 i =<< clang_HTMLStartTag_getAttrName comment i
+            traceU level3 "value" =<< clang_HTMLStartTag_getAttrValue comment i
+
+      Right CXComment_HTMLEndTag -> do
+        traceU level1 "name" =<< clang_HTMLTagComment_getTagName comment
+
+      Right CXComment_Paragraph -> pure ()
+
+      Right CXComment_BlockCommand -> do
+        traceU level1 "name" =<<
+          clang_BlockCommandComment_getCommandName comment
+        numArgs <- clang_BlockCommandComment_getNumArgs comment
+        when (numArgs > 0) $ do
+          traceU level1 "args" numArgs
+          forM_ [0 .. numArgs - 1] $ \i ->
+            traceO_ level2 i =<< clang_BlockCommandComment_getArgText comment i
+        -- NOTE paragraph is also provided by children
+        -- para <- clang_BlockCommandComment_getParagraph comment
+        -- traceL level1 "paragraph"
+        -- dumpComment level2 Nothing para
+
+      Right CXComment_ParamCommand -> do
+        traceU level1 "name" =<< clang_ParamCommandComment_getParamName comment
+        traceU level1 "index"
+          =<< clang_ParamCommandComment_getParamIndex comment
+        traceUnless level2 "invalid"
+          =<< clang_ParamCommandComment_isParamIndexValid comment
+        traceU level1 "direction" . fromSimpleEnum
+          =<< clang_ParamCommandComment_getDirection comment
+        traceU level2 "explicit"
+          =<< clang_ParamCommandComment_isDirectionExplicit comment
+
+      Right CXComment_TParamCommand -> do
+        traceU level1 "name" =<< clang_TParamCommandComment_getParamName comment
+        traceUnless level1 "position invalid"
+          =<< clang_TParamCommandComment_isParamPositionValid comment
+        depth <- clang_TParamCommandComment_getDepth comment
+        traceU level1 "depth" depth
+        forM_ [0 .. depth] $ \i ->
+          traceO level2 i "index"
+            =<< clang_TParamCommandComment_getIndex comment i
+
+      Right CXComment_VerbatimBlockCommand -> pure ()
+
+      Right CXComment_VerbatimBlockLine ->
+        traceU level1 "text" =<< clang_VerbatimBlockLineComment_getText comment
+
+      Right CXComment_VerbatimLine ->
+        traceU level1 "text" =<< clang_VerbatimLineComment_getText comment
+
+      Right CXComment_FullComment -> pure ()
+
+      Left n  -> traceU level1 "COMMENT_KIND_ENUM_OUT_OF_RANGE" n
+
+    numChildren <- clang_Comment_getNumChildren comment
+    when (numChildren > 0) $ do
+      traceU level1 "children" numChildren
+      forM_ [0 .. numChildren - 1] $ \i ->
         dumpComment level2 (Just i) =<< clang_Comment_getChild comment i
   where
-    level1, level2 :: Int
+    level1, level2, level3 :: Int
     level1 = level + 1
     level2 = level + 2
+    level3 = level + 3
 
 {-------------------------------------------------------------------------------
   Helper Functions
@@ -167,12 +233,15 @@ traceO level index label value =
 traceWhen :: Int -> String -> Bool -> IO ()
 traceWhen level label b = when b $ traceU level label b
 
+traceUnless :: Int -> String -> Bool -> IO ()
+traceUnless level label b = unless b $ traceU level label b
+
 {-------------------------------------------------------------------------------
   CLI
 -------------------------------------------------------------------------------}
 
-parseArgs :: IO Options
-parseArgs = OA.execParser pinfo
+main :: IO ()
+main = clangAstDump =<< OA.execParser pinfo
   where
     pinfo :: OA.ParserInfo Options
     pinfo = OA.info (OA.helper <*> parseOptions) $ mconcat
@@ -200,6 +269,3 @@ parseArgs = OA.execParser pinfo
 
     mkFlag :: String -> String -> OA.Parser Bool
     mkFlag flag doc = OA.switch $ OA.long flag <> OA.help doc
-
-main :: IO ()
-main = clangAstDump =<< parseArgs
