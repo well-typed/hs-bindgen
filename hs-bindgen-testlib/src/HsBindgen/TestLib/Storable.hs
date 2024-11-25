62a6d0e7b686a5e04eb44948abe408566e2bc526
Travis Cardwell <travis.cardwell@extrema.is>
2024-11-25 11:05:05 +0900

    Update Storable properties/assertions

--- hs-bindgen-testlib/src/HsBindgen/TestLib/Storable.hs
+++ hs-bindgen-testlib/src/HsBindgen/TestLib/Storable.hs
@@ -1,23 +1,40 @@
 module HsBindgen.TestLib.Storable (
     -- * Properties
-    prop_PokePeekSameSemantics
+    namePokePeekXSameSemanticsX
+  , prop_PokePeekXSameSemanticsX
+  , assertPokePeekXSameSemanticsX
   ) where
 
 import Foreign qualified as F
 import Test.QuickCheck.Monadic qualified as QCM
+import Test.Tasty.HUnit (Assertion)
 import Test.Tasty.QuickCheck (Property)
 
-import HsBindgen.TestLib.SameSemantics (SameSemantics(sameSemantics))
+import HsBindgen.TestLib.SameSemantics ((@==~?), SameSemantics(sameSemantics))
 
 {-------------------------------------------------------------------------------
   Properties
 -------------------------------------------------------------------------------}
 
--- | A value that is `F.poke`ed and then `F.peek`ed is semantically equal to the
--- orignal value
-prop_PokePeekSameSemantics :: (F.Storable a, SameSemantics a) => a -> Property
-prop_PokePeekSameSemantics x = QCM.monadicIO $ do
+-- | The 'F.poke' then 'F.peek' of a value is semantically equal to the value
+namePokePeekXSameSemanticsX :: String
+namePokePeekXSameSemanticsX = "PokePeekXSameSemanticsX"
+
+-- | The 'F.poke' then 'F.peek' of a value is semantically equal to the value
+prop_PokePeekXSameSemanticsX :: (F.Storable a, SameSemantics a) => a -> Property
+prop_PokePeekXSameSemanticsX x = QCM.monadicIO $ do
     x' <- QCM.run . F.alloca $ \ptr -> do
       F.poke ptr x
       F.peek ptr
     QCM.assert $ x' `sameSemantics` x
+
+-- | The 'F.poke' then 'F.peek' of a value is semantically equal to the value
+assertPokePeekXSameSemanticsX ::
+       (F.Storable a, SameSemantics a, Show a)
+    => a
+    -> Assertion
+assertPokePeekXSameSemanticsX x = do
+    x' <- F.alloca $ \ptr -> do
+      F.poke ptr x
+      F.peek ptr
+    x @==~? x'
