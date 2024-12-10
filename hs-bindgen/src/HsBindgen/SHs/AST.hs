00044d035c6d6d81f49b7153c983101fecddcaf9
Travis Cardwell <travis.cardwell@extrema.is>
2024-12-11 08:18:59 +0900

    Add origin info to SHs AST
    At this point, there are no differences with the Hs AST, so those types
    are used as-is.  If we ever introduce transformations, we should define
    SHs origin types that record the transformations made.

--- hs-bindgen/src/HsBindgen/SHs/AST.hs
+++ hs-bindgen/src/HsBindgen/SHs/AST.hs
@@ -18,6 +18,7 @@ module HsBindgen.SHs.AST (
 
 import HsBindgen.Imports
 import HsBindgen.NameHint
+import HsBindgen.Hs.AST qualified as Hs
 import HsBindgen.Hs.AST.Name
 import HsBindgen.Hs.AST.Type
 
@@ -152,8 +153,9 @@ data Instance  = Instance {
   deriving stock (Show)
 
 data Field = Field {
-      fieldName :: HsName NsVar
-    , fieldType :: ClosedType
+      fieldName   :: HsName NsVar
+    , fieldType   :: ClosedType
+    , fieldOrigin :: Hs.FieldOrigin
     }
   deriving stock (Show)
 
@@ -161,6 +163,7 @@ data Record = Record {
       dataType   :: HsName NsTypeConstr
     , dataCon    :: HsName NsConstr
     , dataFields :: [Field]
+    , dataOrigin :: Hs.StructOrigin
     }
   deriving stock (Show)
 
@@ -168,6 +171,7 @@ data Newtype = Newtype {
       newtypeName   :: HsName NsTypeConstr
     , newtypeCon    :: HsName NsConstr
     , newtypeField  :: Field
+    , newtypeOrigin :: Hs.NewtypeOrigin
     }
   deriving stock (Show)
 
@@ -176,12 +180,14 @@ data ForeignImport = ForeignImport
     , foreignImportType     :: ClosedType
     , foreignImportOrigName :: Text
     , foreignImportHeader   :: FilePath -- TODO: https://github.com/well-typed/hs-bindgen/issues/333
+    , foreignImportOrigin   :: Hs.ForeignImportDeclOrigin
     }
   deriving stock (Show)
 
 data PatternSynonym = PatternSynonym
-    { patSynName :: HsName NsConstr
-    , patSynType :: ClosedType
-    , patSynRHS  :: ClosedExpr -- TODO: This should be Pat(tern)
+    { patSynName   :: HsName NsConstr
+    , patSynType   :: ClosedType
+    , patSynRHS    :: ClosedExpr -- TODO: This should be Pat(tern)
+    , patSynOrigin :: Hs.PatSynOrigin
     }
   deriving stock (Show)
