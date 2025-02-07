fd5cf9f9b77703f5b02a3b20fe41a494a5a2b9b1
Travis Cardwell <travis.cardwell@extrema.is>
2025-02-07 20:59:50 +0900

    Hack ToExpr SingleLoc purely
    This version makes absolute paths to `musl-include` headers relative
    without IO.

--- hs-bindgen/tests/Orphans.hs
+++ hs-bindgen/tests/Orphans.hs
@@ -10,10 +10,8 @@ import Data.TreeDiff.OMap qualified as OMap
 import Data.Vec.Lazy (Vec)
 import Data.Vec.Lazy qualified as Vec
 import Foreign.C
-import System.Directory qualified as Dir
-import System.FilePath (makeRelative, splitDirectories)
+import System.FilePath (isRelative, splitDirectories)
 import System.FilePath.Posix qualified as Posix
-import System.IO.Unsafe (unsafePerformIO)
 
 import HsBindgen.C.AST qualified as C
 import HsBindgen.C.Tc.Macro as CMacro
@@ -74,10 +72,18 @@ instance ToExpr a => ToExpr (C.Token a)
 -- do not use record syntax, as it's very verbose
 instance ToExpr C.SingleLoc where
   toExpr (C.SingleLoc p l c) = toExpr $
-    -- use posix directory separators even on windows
-    Posix.joinPath (splitDirectories (makeRelative cwd (Text.unpack (C.getSourcePath p)))) ++ ":" ++
-    show l ++ ":" ++
-    show c
+    let nativePath = Text.unpack $ C.getSourcePath p
+        dirs = splitDirectories nativePath
+        -- use posix directory separators even on windows
+        normalizedPath = Posix.joinPath $
+          if isRelative nativePath
+            then dirs
+            else
+              -- make relative to musl-include when applicable
+              case dropWhile (/= "musl-include") dirs of
+                []    -> dirs
+                dirs' -> dirs'
+    in  normalizedPath ++ ":" ++ show l ++ ":" ++ show c
 
 instance ToExpr C.ReparseError where
   toExpr C.ReparseError {..} = Expr.Rec "ReparseError" $ OMap.fromList
@@ -309,16 +315,3 @@ instance ToExpr (Idx j) where
 
 instance ToExpr (Size ctx) where
   toExpr size = Expr.App "Size" [toExpr (sizeToInt size)]
-
-{-------------------------------------------------------------------------------
-  Dirty Hacks
--------------------------------------------------------------------------------}
-
--- | Unsafely get the current working directory
---
--- We need to use relative paths in test fixtures since absolute paths differ
--- across different systems.  Paths are only compared via 'ToExpr C.SingleLoc`,
--- and a minimal hack allows us to transform absolute paths to relative paths
--- there.
-cwd :: FilePath
-cwd = unsafePerformIO Dir.getCurrentDirectory
