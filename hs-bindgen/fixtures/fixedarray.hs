5c0f0596580ee6adbe497de7078ad655e3dcb004
Oleg Grenrus <oleg.grenrus@iki.fi>
2025-02-11 12:50:54 +0200

    Add example for nexted constant size arrays
    Resolves #59

--- hs-bindgen/fixtures/fixedarray.hs
+++ hs-bindgen/fixtures/fixedarray.hs
@@ -30,4 +30,379 @@
     Storable
     (HsName
       "@NsTypeConstr"
-      "Triple")]
+      "Triple"),
+  DeclData
+    Struct {
+      structName = HsName
+        "@NsTypeConstr"
+        "Example",
+      structConstr = HsName
+        "@NsConstr"
+        "Example",
+      structFields = [
+        Field {
+          fieldName = HsName
+            "@NsVar"
+            "example_triple",
+          fieldType = HsConstArray
+            3
+            (HsPrimType HsPrimCInt),
+          fieldOrigin =
+          FieldOriginStructField
+            StructField {
+              fieldName = CName "triple",
+              fieldOffset = 0,
+              fieldWidth = Nothing,
+              fieldType = TypeConstArray
+                3
+                (TypePrim
+                  (PrimIntegral PrimInt Signed)),
+              fieldSourceLoc =
+              "examples/fixedarray.h:4:9"}},
+        Field {
+          fieldName = HsName
+            "@NsVar"
+            "example_sudoku",
+          fieldType = HsConstArray
+            3
+            (HsConstArray
+              3
+              (HsPrimType HsPrimCInt)),
+          fieldOrigin =
+          FieldOriginStructField
+            StructField {
+              fieldName = CName "sudoku",
+              fieldOffset = 96,
+              fieldWidth = Nothing,
+              fieldType = TypeConstArray
+                3
+                (TypeConstArray
+                  3
+                  (TypePrim
+                    (PrimIntegral PrimInt Signed))),
+              fieldSourceLoc =
+              "examples/fixedarray.h:5:9"}}],
+      structOrigin =
+      StructOriginStruct
+        Struct {
+          structDeclPath = DeclPathStruct
+            (DeclNameTag (CName "Example"))
+            DeclPathTop,
+          structSizeof = 48,
+          structAlignment = 4,
+          structFields = [
+            StructField {
+              fieldName = CName "triple",
+              fieldOffset = 0,
+              fieldWidth = Nothing,
+              fieldType = TypeConstArray
+                3
+                (TypePrim
+                  (PrimIntegral PrimInt Signed)),
+              fieldSourceLoc =
+              "examples/fixedarray.h:4:9"},
+            StructField {
+              fieldName = CName "sudoku",
+              fieldOffset = 96,
+              fieldWidth = Nothing,
+              fieldType = TypeConstArray
+                3
+                (TypeConstArray
+                  3
+                  (TypePrim
+                    (PrimIntegral PrimInt Signed))),
+              fieldSourceLoc =
+              "examples/fixedarray.h:5:9"}],
+          structFlam = Nothing,
+          structSourceLoc =
+          "examples/fixedarray.h:3:8"}},
+  DeclInstance
+    (InstanceStorable
+      Struct {
+        structName = HsName
+          "@NsTypeConstr"
+          "Example",
+        structConstr = HsName
+          "@NsConstr"
+          "Example",
+        structFields = [
+          Field {
+            fieldName = HsName
+              "@NsVar"
+              "example_triple",
+            fieldType = HsConstArray
+              3
+              (HsPrimType HsPrimCInt),
+            fieldOrigin =
+            FieldOriginStructField
+              StructField {
+                fieldName = CName "triple",
+                fieldOffset = 0,
+                fieldWidth = Nothing,
+                fieldType = TypeConstArray
+                  3
+                  (TypePrim
+                    (PrimIntegral PrimInt Signed)),
+                fieldSourceLoc =
+                "examples/fixedarray.h:4:9"}},
+          Field {
+            fieldName = HsName
+              "@NsVar"
+              "example_sudoku",
+            fieldType = HsConstArray
+              3
+              (HsConstArray
+                3
+                (HsPrimType HsPrimCInt)),
+            fieldOrigin =
+            FieldOriginStructField
+              StructField {
+                fieldName = CName "sudoku",
+                fieldOffset = 96,
+                fieldWidth = Nothing,
+                fieldType = TypeConstArray
+                  3
+                  (TypeConstArray
+                    3
+                    (TypePrim
+                      (PrimIntegral PrimInt Signed))),
+                fieldSourceLoc =
+                "examples/fixedarray.h:5:9"}}],
+        structOrigin =
+        StructOriginStruct
+          Struct {
+            structDeclPath = DeclPathStruct
+              (DeclNameTag (CName "Example"))
+              DeclPathTop,
+            structSizeof = 48,
+            structAlignment = 4,
+            structFields = [
+              StructField {
+                fieldName = CName "triple",
+                fieldOffset = 0,
+                fieldWidth = Nothing,
+                fieldType = TypeConstArray
+                  3
+                  (TypePrim
+                    (PrimIntegral PrimInt Signed)),
+                fieldSourceLoc =
+                "examples/fixedarray.h:4:9"},
+              StructField {
+                fieldName = CName "sudoku",
+                fieldOffset = 96,
+                fieldWidth = Nothing,
+                fieldType = TypeConstArray
+                  3
+                  (TypeConstArray
+                    3
+                    (TypePrim
+                      (PrimIntegral PrimInt Signed))),
+                fieldSourceLoc =
+                "examples/fixedarray.h:5:9"}],
+            structFlam = Nothing,
+            structSourceLoc =
+            "examples/fixedarray.h:3:8"}}
+      StorableInstance {
+        storableSizeOf = 48,
+        storableAlignment = 4,
+        storablePeek = Lambda
+          (NameHint "ptr")
+          (Ap
+            (StructCon
+              Struct {
+                structName = HsName
+                  "@NsTypeConstr"
+                  "Example",
+                structConstr = HsName
+                  "@NsConstr"
+                  "Example",
+                structFields = [
+                  Field {
+                    fieldName = HsName
+                      "@NsVar"
+                      "example_triple",
+                    fieldType = HsConstArray
+                      3
+                      (HsPrimType HsPrimCInt),
+                    fieldOrigin =
+                    FieldOriginStructField
+                      StructField {
+                        fieldName = CName "triple",
+                        fieldOffset = 0,
+                        fieldWidth = Nothing,
+                        fieldType = TypeConstArray
+                          3
+                          (TypePrim
+                            (PrimIntegral PrimInt Signed)),
+                        fieldSourceLoc =
+                        "examples/fixedarray.h:4:9"}},
+                  Field {
+                    fieldName = HsName
+                      "@NsVar"
+                      "example_sudoku",
+                    fieldType = HsConstArray
+                      3
+                      (HsConstArray
+                        3
+                        (HsPrimType HsPrimCInt)),
+                    fieldOrigin =
+                    FieldOriginStructField
+                      StructField {
+                        fieldName = CName "sudoku",
+                        fieldOffset = 96,
+                        fieldWidth = Nothing,
+                        fieldType = TypeConstArray
+                          3
+                          (TypeConstArray
+                            3
+                            (TypePrim
+                              (PrimIntegral PrimInt Signed))),
+                        fieldSourceLoc =
+                        "examples/fixedarray.h:5:9"}}],
+                structOrigin =
+                StructOriginStruct
+                  Struct {
+                    structDeclPath = DeclPathStruct
+                      (DeclNameTag (CName "Example"))
+                      DeclPathTop,
+                    structSizeof = 48,
+                    structAlignment = 4,
+                    structFields = [
+                      StructField {
+                        fieldName = CName "triple",
+                        fieldOffset = 0,
+                        fieldWidth = Nothing,
+                        fieldType = TypeConstArray
+                          3
+                          (TypePrim
+                            (PrimIntegral PrimInt Signed)),
+                        fieldSourceLoc =
+                        "examples/fixedarray.h:4:9"},
+                      StructField {
+                        fieldName = CName "sudoku",
+                        fieldOffset = 96,
+                        fieldWidth = Nothing,
+                        fieldType = TypeConstArray
+                          3
+                          (TypeConstArray
+                            3
+                            (TypePrim
+                              (PrimIntegral PrimInt Signed))),
+                        fieldSourceLoc =
+                        "examples/fixedarray.h:5:9"}],
+                    structFlam = Nothing,
+                    structSourceLoc =
+                    "examples/fixedarray.h:3:8"}})
+            [
+              PeekByteOff (Idx 0) 0,
+              PeekByteOff (Idx 0) 12]),
+        storablePoke = Lambda
+          (NameHint "ptr")
+          (Lambda
+            (NameHint "s")
+            (ElimStruct
+              (Idx 0)
+              Struct {
+                structName = HsName
+                  "@NsTypeConstr"
+                  "Example",
+                structConstr = HsName
+                  "@NsConstr"
+                  "Example",
+                structFields = [
+                  Field {
+                    fieldName = HsName
+                      "@NsVar"
+                      "example_triple",
+                    fieldType = HsConstArray
+                      3
+                      (HsPrimType HsPrimCInt),
+                    fieldOrigin =
+                    FieldOriginStructField
+                      StructField {
+                        fieldName = CName "triple",
+                        fieldOffset = 0,
+                        fieldWidth = Nothing,
+                        fieldType = TypeConstArray
+                          3
+                          (TypePrim
+                            (PrimIntegral PrimInt Signed)),
+                        fieldSourceLoc =
+                        "examples/fixedarray.h:4:9"}},
+                  Field {
+                    fieldName = HsName
+                      "@NsVar"
+                      "example_sudoku",
+                    fieldType = HsConstArray
+                      3
+                      (HsConstArray
+                        3
+                        (HsPrimType HsPrimCInt)),
+                    fieldOrigin =
+                    FieldOriginStructField
+                      StructField {
+                        fieldName = CName "sudoku",
+                        fieldOffset = 96,
+                        fieldWidth = Nothing,
+                        fieldType = TypeConstArray
+                          3
+                          (TypeConstArray
+                            3
+                            (TypePrim
+                              (PrimIntegral PrimInt Signed))),
+                        fieldSourceLoc =
+                        "examples/fixedarray.h:5:9"}}],
+                structOrigin =
+                StructOriginStruct
+                  Struct {
+                    structDeclPath = DeclPathStruct
+                      (DeclNameTag (CName "Example"))
+                      DeclPathTop,
+                    structSizeof = 48,
+                    structAlignment = 4,
+                    structFields = [
+                      StructField {
+                        fieldName = CName "triple",
+                        fieldOffset = 0,
+                        fieldWidth = Nothing,
+                        fieldType = TypeConstArray
+                          3
+                          (TypePrim
+                            (PrimIntegral PrimInt Signed)),
+                        fieldSourceLoc =
+                        "examples/fixedarray.h:4:9"},
+                      StructField {
+                        fieldName = CName "sudoku",
+                        fieldOffset = 96,
+                        fieldWidth = Nothing,
+                        fieldType = TypeConstArray
+                          3
+                          (TypeConstArray
+                            3
+                            (TypePrim
+                              (PrimIntegral PrimInt Signed))),
+                        fieldSourceLoc =
+                        "examples/fixedarray.h:5:9"}],
+                    structFlam = Nothing,
+                    structSourceLoc =
+                    "examples/fixedarray.h:3:8"}}
+              (Add 2)
+              (Seq
+                [
+                  PokeByteOff (Idx 3) 0 (Idx 0),
+                  PokeByteOff
+                    (Idx 3)
+                    12
+                    (Idx 1)])))}),
+  DeclNewtypeInstance
+    DeriveStock
+    Show
+    (HsName
+      "@NsTypeConstr"
+      "Example"),
+  DeclNewtypeInstance
+    DeriveStock
+    Eq
+    (HsName
+      "@NsTypeConstr"
+      "Example")]
--- hs-bindgen/fixtures/fixedarray.pp.hs
+++ hs-bindgen/fixtures/fixedarray.pp.hs
@@ -8,9 +8,39 @@ module Example where
 import qualified Foreign as F
 import qualified Foreign.C as FC
 import qualified HsBindgen.Runtime.ConstantArray
+import Prelude ((<*>), (>>), Eq, Int, Show, pure)
 
 newtype Triple = Triple
   { unTriple :: (HsBindgen.Runtime.ConstantArray.ConstantArray 3) FC.CInt
   }
 
 deriving newtype instance F.Storable Triple
+
+data Example = Example
+  { example_triple :: (HsBindgen.Runtime.ConstantArray.ConstantArray 3) FC.CInt
+  , example_sudoku :: (HsBindgen.Runtime.ConstantArray.ConstantArray 3) ((HsBindgen.Runtime.ConstantArray.ConstantArray 3) FC.CInt)
+  }
+
+instance F.Storable Example where
+
+  sizeOf = \_ -> (48 :: Int)
+
+  alignment = \_ -> (4 :: Int)
+
+  peek =
+    \ptr0 ->
+          pure Example
+      <*> F.peekByteOff ptr0 (0 :: Int)
+      <*> F.peekByteOff ptr0 (12 :: Int)
+
+  poke =
+    \ptr0 ->
+      \s1 ->
+        case s1 of
+          Example example_triple2 example_sudoku3 ->
+               F.pokeByteOff ptr0 (0 :: Int) example_triple2
+            >> F.pokeByteOff ptr0 (12 :: Int) example_sudoku3
+
+deriving stock instance Show Example
+
+deriving stock instance Eq Example
