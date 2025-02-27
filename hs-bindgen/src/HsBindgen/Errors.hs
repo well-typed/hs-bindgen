bd67f9dbe7e126644f630d8efc5c232ade9e3393
Oleg Grenrus <oleg.grenrus@iki.fi>
2025-02-27 17:12:31 +0200

    Export HsBindgenException conversion functions

--- hs-bindgen/src/HsBindgen/Errors.hs
+++ hs-bindgen/src/HsBindgen/Errors.hs
@@ -1,5 +1,7 @@
 module HsBindgen.Errors (
     HsBindgenException (..),
+    hsBindgenExceptionToException,
+    hsBindgenExceptionFromException,
     TODOException (..),
     throwPure_TODO,
     PanicException,
