0fcee927120435b65f449b4eb3a4470a6077b541
Edsko de Vries <edsko@well-typed.com>
2025-04-09 17:47:13 +0200

    Deal with awkward names
    Closes #562
    Closes #563

--- hs-bindgen/fixtures/adios.pp.hs
+++ hs-bindgen/fixtures/adios.pp.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CApiFFI #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NoImplicitPrelude #-}
@@ -10,7 +11,13 @@ import qualified Data.Bits as Bits
 import qualified Data.Ix as Ix
 import qualified Foreign as F
 import qualified Foreign.C as FC
-import Prelude (Bounded, Enum, Eq, Integral, Num, Ord, Read, Real, Show)
+import Prelude (Bounded, Enum, Eq, IO, Integral, Num, Ord, Read, Real, Show)
+
+foreign import capi safe "adios.h ϒ" cϒ :: IO ()
+
+foreign import capi safe "adios.h 拜拜" 拜拜 :: IO ()
+
+foreign import capi safe "adios.h Say拜拜" say拜拜 :: IO ()
 
 newtype Adio'0301s = Adio'0301s
   { un_Adio'0301s :: FC.CInt
@@ -41,3 +48,33 @@ deriving newtype instance Integral Adio'0301s
 deriving newtype instance Num Adio'0301s
 
 deriving newtype instance Real Adio'0301s
+
+newtype C数字 = C数字
+  { un_C数字 :: FC.CInt
+  }
+
+deriving newtype instance F.Storable C数字
+
+deriving stock instance Eq C数字
+
+deriving stock instance Ord C数字
+
+deriving stock instance Read C数字
+
+deriving stock instance Show C数字
+
+deriving newtype instance Enum C数字
+
+deriving newtype instance Ix.Ix C数字
+
+deriving newtype instance Bounded C数字
+
+deriving newtype instance Bits.Bits C数字
+
+deriving newtype instance FiniteBits C数字
+
+deriving newtype instance Integral C数字
+
+deriving newtype instance Num C数字
+
+deriving newtype instance Real C数字
