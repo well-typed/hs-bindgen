2317a08f12e20c33c51f08634b51b26446be000e
Oleg Grenrus <oleg.grenrus@iki.fi>
2025-04-11 17:29:47 +0300

    Less reserved names

---
+++ hs-bindgen/examples/names.h
@@ -0,0 +1,29 @@
+// Names we expect to be escaped (`ghcExtensionKeywords`)
+// NOTE: `static` is (also) a C keyword and so cannot appear here.
+void by(void);
+void forall(void);
+void mdo(void);
+void pattern(void);
+void proc(void);
+void rec(void);
+void using(void);
+
+// Names we expect _not_ to be escaped (`ghcNonReservedKeywords`)
+void anyclass(void);
+void capi(void);
+void cases(void);
+void ccall(void);
+void dynamic(void);
+void export(void);
+void family(void);
+void group(void);
+void interruptible(void);
+void javascript(void);
+void label(void);
+void prim(void);
+void role(void);
+void safe(void);
+void stdcall(void);
+void stock(void);
+void unsafe(void);
+void via(void);
---
+++ hs-bindgen/fixtures/names.hs
@@ -0,0 +1,513 @@
+[
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "by'",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "by",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "by",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:3:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "forall'",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "forall",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "forall",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:4:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "mdo'",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "mdo",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "mdo",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:5:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "pattern'",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "pattern",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "pattern",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:6:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "proc'",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "proc",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "proc",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:7:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "rec'",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "rec",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "rec",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:8:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "using'",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "using",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "using",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:9:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "anyclass",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "anyclass",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "anyclass",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:12:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "capi",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "capi",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "capi",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:13:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "cases",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "cases",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "cases",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:14:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "ccall",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "ccall",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "ccall",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:15:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "dynamic",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "dynamic",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "dynamic",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:16:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "export",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "export",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "export",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:17:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "family",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "family",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "family",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:18:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "group",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "group",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "group",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:19:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "interruptible",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "interruptible",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName
+            "interruptible",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:20:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "javascript",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "javascript",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName
+            "javascript",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:21:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "label",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "label",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "label",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:22:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "prim",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "prim",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "prim",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:23:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "role",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "role",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "role",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:24:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "safe",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "safe",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "safe",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:25:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "stdcall",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "stdcall",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "stdcall",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:26:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "stock",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "stock",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "stock",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:27:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "unsafe",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName =
+      "unsafe",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "unsafe",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:28:6"}},
+  DeclForeignImport
+    ForeignImportDecl {
+      foreignImportName = HsName
+        "@NsVar"
+        "via",
+      foreignImportType = HsIO
+        (HsPrimType HsPrimUnit),
+      foreignImportCRes = TypeVoid,
+      foreignImportCArgs = [],
+      foreignImportOrigName = "via",
+      foreignImportHeader = "names.h",
+      foreignImportDeclOrigin =
+      ForeignImportDeclOriginFunction
+        Function {
+          functionName = CName "via",
+          functionArgs = [],
+          functionRes = TypeVoid,
+          functionHeader = "names.h",
+          functionSourceLoc =
+          "names.h:29:6"}}]
