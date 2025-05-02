716bd7bd1a298cc1fd937134656ef315fb992011
Dominik Schrempf <dominik.schrempf@gmail.com>
2025-05-02 12:54:47 +0200

    Update Nix Flake and add minimal CI
    (CI only works with CACHIX token).

--- flake.nix
+++ flake.nix
@@ -14,48 +14,83 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        ghcVersion = "ghc96";
+        rBindgenVersion = "0.70.1";
+        hsBindgenPkgNames = [
+          "ansi-diff"
+          "c-expr"
+          "clang"
+          "hs-bindgen"
+          "hs-bindgen-runtime"
+          "hs-bindgen-test-runtime"
+          "userland-capi"
+        ];
+        hMkPackage = { callCabal2nix, ... }: name: callCabal2nix name (./. + "/${name}") { };
+        hOverlay = nfinal: nprev: {
+          haskell = nprev.haskell // {
+            packageOverrides =
+              hfinal: hprev:
+              nprev.haskell.packageOverrides hfinal hprev
+              // nixpkgs.lib.genAttrs hsBindgenPkgNames (hMkPackage hfinal)
+              // {
+                debruijn = nfinal.haskell.lib.markUnbroken hprev.debruijn;
+                fin = hprev.fin_0_3_2;
+                universe-base = hprev.universe-base_1_1_4;
+              };
+          };
+        };
+        # See ./test/internal/Test/Internal/Rust.hs::rustBindgenVersion.
+        rOverlay = final: prev: {
+          rust-bindgen-unwrapped = prev.rust-bindgen-unwrapped.overrideAttrs (old: rec {
+            version = rBindgenVersion;
+            src = prev.fetchCrate {
+              pname = "bindgen-cli";
+              inherit version;
+              hash = "sha256-6FRcW/VGqlmLjb64UYqk21HmQ8u0AdVD3S2F+9D/vQo=";
+            };
+            cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+              pname = old.pname;
+              inherit version src;
+              hash = "sha256-r4ZI+uybK3MzJMYlRwmNhZMBO3aMKCIIznOOdQ0ReqU=";
+            };
+          });
+        };
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            (final: prev: {
-              # See ./test/internal/Test/Internal/Rust.hs::rustBindgenVersion.
-              rust-bindgen-unwrapped = prev.rust-bindgen-unwrapped.overrideAttrs (old: rec {
-                version = "0.70.1";
-                src = prev.fetchCrate {
-                  pname = "bindgen-cli";
-                  inherit version;
-                  hash = "sha256-6FRcW/VGqlmLjb64UYqk21HmQ8u0AdVD3S2F+9D/vQo=";
-                };
-                cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-                  pname = old.pname;
-                  inherit version src;
-                  hash = "sha256-r4ZI+uybK3MzJMYlRwmNhZMBO3aMKCIIznOOdQ0ReqU=";
-                };
-              });
-            })
+            rOverlay
+            hOverlay
           ];
         };
-        hpkgs = pkgs.haskellPackages;
+        hpkgs = pkgs.haskell.packages.${ghcVersion};
         lpkgs = pkgs.llvmPackages;
+        hsBindgenPkgs = nixpkgs.lib.genAttrs hsBindgenPkgNames (n: hpkgs.${n});
       in
       {
-        devShells.default = (pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }) {
-          buildInputs = [ lpkgs.libclang ];
-          packages = [
+        packages = hsBindgenPkgs // {
+          # Does not build yet, because package versions in Nixpkgs are outdated.
+          default = hsBindgenPkgs.hs-bindgen;
+        };
+        devShells.default = hpkgs.shellFor {
+          packages = _: (builtins.attrValues hsBindgenPkgs);
+          nativeBuildInputs = [
             # Haskell.
             hpkgs.cabal-install
             hpkgs.ghc
             hpkgs.haskell-language-server
-
-            # Other.
-            lpkgs.clang
-            lpkgs.llvm
+            # Rust.
             pkgs.rust-bindgen
+            # Clang.
+            lpkgs.clang
+            lpkgs.libclang
+            lpkgs.llvm
           ];
+          doBenchmark = true;
+          withHoogle = true;
           shellHook = ''
-            # When running `clang` during GHC compilation with Template Haskell,
-            # the Nixpkgs wrapper does not work, and so we need to set include
-            # paths manually.
+            # When accessing the `libclang` interface during GHC compilation
+            # with Template Haskell, the Nixpkgs wrapper does not work, and so
+            # we need to set include paths manually.
             C_INCLUDE_PATH="$(< ${lpkgs.clang}/nix-support/orig-libc-dev)/include:$(< ${pkgs.gcc}/nix-support/orig-cc)/lib/gcc/x86_64-unknown-linux-gnu/14.2.1/include"
             export C_INCLUDE_PATH
             # `rust-bindgen` allows usage of environment variables to define
