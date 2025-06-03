c2830914b23faf66c28b008f0f06a0aa819d6d3a
Oleg Grenrus <oleg.grenrus@iki.fi>
2025-06-03 19:04:08 +0300

    Move SigmaType (and its parts) to own module

--- hs-bindgen/src-internal/HsBindgen/Hs/AST.hs
+++ hs-bindgen/src-internal/HsBindgen/Hs/AST.hs
@@ -63,17 +63,16 @@ import Data.Type.Nat qualified as Nat
 import DeBruijn (Ctx, EmptyCtx, Wk (..), Add (..), Idx (..))
 
 import HsBindgen.C.AST qualified as C
-import HsBindgen.C.Tc.Macro qualified as Macro
 
 import HsBindgen.ExtBindings (HsTypeClass(..))
 import HsBindgen.Imports
 import HsBindgen.NameHint
 import HsBindgen.Hs.AST.Name
 import HsBindgen.Hs.AST.Type
+import HsBindgen.Hs.AST.SigmaType
 import HsBindgen.Hs.AST.Origin
 import HsBindgen.Hs.AST.Strategy
 import HsBindgen.Orphans ()
-import HsBindgen.Util.TestEquality
 
 import C.Char qualified
 
@@ -193,66 +192,6 @@ data VarDecl =
     , varDeclBody :: VarDeclRHS EmptyCtx
     }
   deriving stock (Generic, Show)
-
--- | A σ-type, of the form @forall tvs. ctxt => body@.
-type SigmaType :: Star
-data SigmaType where
-  ForallTy ::
-    { forallTyBinders :: Vec n NameHint
-    , forallTy        :: PhiType n
-    }
-    -> SigmaType
-
-deriving stock instance Show SigmaType
-
--- | A φ-type, of the form @ctxt => body@.
-type PhiType :: Ctx -> Star
-data PhiType ctx
-  = QuantTy
-  { quantTyCts  :: [PredType ctx]
-  , quantTyBody :: TauType ctx
-  }
-  deriving stock (Generic, Show)
-
--- | A τ-type: no quantification or contexts (i.e. no @forall@, no @=>@ arrows).
-type TauType :: Ctx -> Star
-data TauType ctx
-  = FunTy (TauType ctx) (TauType ctx)
-  | TyVarTy (Idx ctx)
-  | TyConAppTy ATyCon [TauType ctx]
-  deriving stock (Eq, Generic, Show)
-
--- | A predicate/constraint τ-type.
-type PredType :: Ctx -> Star
-data PredType ctx
-  = DictTy AClass [TauType ctx]
-  | NomEqTy (TauType ctx) (TauType ctx)
-  deriving stock Show
-
-instance Eq (PredType ctx) where
-  DictTy cls1 tys1 == DictTy cls2 tys2 =
-    cls1 == cls2 && tys1 == tys2
-  NomEqTy l1 r1 == NomEqTy l2 r2 =
-    l1 == l2 && r1 == r2
-  _ == _ = False
-
-data ATyCon where
-  ATyCon :: Macro.TyCon args Macro.Ty -> ATyCon
-instance Show ATyCon where
-  show ( ATyCon tc ) = show tc
-instance Eq ATyCon where
-  ATyCon tc1 == ATyCon tc2 =
-    isJust $ equals2 tc1 tc2
-
-data AClass where
-  AClass :: Macro.TyCon args Macro.Ct -> AClass
-instance Show AClass where
-  show ( AClass tc ) = show tc
-instance Eq AClass where
-  AClass ( Macro.GenerativeTyCon ( Macro.ClassTyCon cls1 ) )
-    ==
-    AClass ( Macro.GenerativeTyCon ( Macro.ClassTyCon cls2 ) ) =
-      isJust $ equals1 cls1 cls2
 
 -- | RHS of a variable or function declaration.
 type VarDeclRHS :: Ctx -> Star
