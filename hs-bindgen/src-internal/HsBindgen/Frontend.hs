744941b59a4105f9cd542d510392f2b2039b9ea9
Dominik Schrempf <dominik@well-typed.com>
2025-06-18 10:08:08 +0200

    Describe passes in frontend

--- hs-bindgen/src-internal/HsBindgen/Frontend.hs
+++ hs-bindgen/src-internal/HsBindgen/Frontend.hs
@@ -31,6 +31,49 @@ import HsBindgen.Util.Tracer
   Construction
 -------------------------------------------------------------------------------}
 
+-- | Frontend.
+--
+-- Overview of passes (see documentation of 'HsBindgen.Frontend.Pass.IsPass'):
+--
+-- 1. 'Parse'
+-- 2. 'Sort'
+-- 3. 'HandleMacros'
+-- 4. 'NameAnon'
+-- 5. 'ResolveBindingSpec'
+-- 6. 'HandleTypedefs'
+-- 7. 'MangleNames'
+--
+-- Although the passes and their order are subject to change, we have to honor
+-- various constraints:
+--
+-- - 'Sort' must come before the following passes because we need to process
+--   declarations before their uses.
+--
+-- - 'HandleMacros': The macro parser needs to know which things are in scope
+--   (other macros as well as typedefs), so we must process declarations in the
+--   right order; that is, 'handleMacros' must be done after sorting the
+--   declarations.
+--
+--   In principle it could run before or after renaming: macros can neither
+--   refer to nor introduce new anonymous declarations, so the relative ordering
+--   of these two passes does not really matter. However, as part of renaming we
+--   replace typedefs around anonymous structs by named structs:
+--
+--   > typedef struct { .. fields .. } foo;
+--
+--   On the C side however @foo@ must be referred to as @foo@, not @struct foo@;
+--   to avoid confusion, it is therefore cleaner to run macro parsing and
+--   declaration reparsing /prior/ to this transformation.
+--
+-- - 'NameAnon' must come before 'ResolveBindingSpec' because it enables users
+--   to configure anonymous types using our generated names for them.
+--
+-- - 'ResolveBindingSpec' must come before 'HandleTypedefs' to enable users to
+--   configure if a specific typedef is squashed for not.
+--
+-- - 'MangleNames': Name mangling depends on information from the binding spec,
+--   and must therefore happen after 'ResolveBindingSpec'. It could be put into
+--   the 'Hs' phase, but we have to draw the line somewhere.
 processTranslationUnit ::
      Tracer IO (TraceWithCallStack FrontendTrace)
   -> ResolvedBindingSpec
