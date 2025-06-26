2a4935202d18b662a3b1093855f62e5061510dfb
Travis Cardwell <travis@well-typed.com>
2025-06-26 10:29:32 +0900

    Add tests
    From the test cases in #795:
    
    * `tagged.h` already in `simple_structs.h` (`S1`)
    * `tagged_macro.h` is not added yet, as it produces incorrect results
      but does not fail
    * `tagged_attr.h` added to `attributes.h`
    * `tagged_attr_macro.h` added to `attributes.h`
    * `anon.h` already in `simple_structs.h` (`S3_t`)
    * `anon_attr.h` added to `attributes.h`
    * `anon_attr_macro.h` added to `attributes.h`

--- hs-bindgen/examples/golden/attributes.h
+++ hs-bindgen/examples/golden/attributes.h
@@ -4,9 +4,35 @@
  * See https://clang.llvm.org/docs/AttributeReference.html
  */
 
+#define PACKED __attribute__((__packed__))
+
+// named struct with attribute
+struct __attribute__((__packed__)) foo {
+    char c;
+    int  i;
+};
+
+// named struct with macro attribute
+struct PACKED bar {
+    char c;
+    int  i;
+};
+
+// anonymous struct with attribute
+typedef struct __attribute__((__packed__)) {
+    char c;
+    int  i;
+} baz;
+
+// anonymous struct with macro attribute
+typedef struct PACKED {
+    char c;
+    int  i;
+} qux;
+
 // Extract from _stdio.h on OSX
 typedef struct __sFILE {
     int _r;
     int _w;
-	int	(* _Nullable _close)(void *);
+    int	(* _Nullable _close)(void *);
 } FILE;
