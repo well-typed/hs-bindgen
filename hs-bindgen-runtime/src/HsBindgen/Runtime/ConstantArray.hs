d3af634db4c83d92dc357f76df9c1d38280c06d1
Edsko de Vries <edsko@well-typed.com>
2025-06-28 07:46:34 +0200

    Code layout

--- hs-bindgen-runtime/src/HsBindgen/Runtime/ConstantArray.hs
+++ hs-bindgen-runtime/src/HsBindgen/Runtime/ConstantArray.hs
@@ -1,44 +1,35 @@
 {-# LANGUAGE ViewPatterns #-}
 module HsBindgen.Runtime.ConstantArray (
-    ConstantArray,
-    repeat,
-    toList,
-    withPtr,
-) where
+    ConstantArray
+    -- * Construction
+  , repeat
+    -- * Query
+  , toList
+  , withPtr
+  ) where
 
-import Prelude (IO, ($), Eq, Show, Int, undefined, fromIntegral, (*), return)
-import GHC.TypeNats (KnownNat, Nat, natVal)
+import Prelude hiding (repeat)
+
 import Data.Coerce (Coercible, coerce)
 import Data.Proxy (Proxy (..))
-import Foreign.Ptr (Ptr, castPtr)
-import Foreign.ForeignPtr (withForeignPtr, mallocForeignPtr)
-import Foreign.Storable (Storable (..))
-import Foreign.Marshal.Utils (copyBytes)
 import Data.Vector.Storable qualified as VS
+import Foreign.ForeignPtr (withForeignPtr, mallocForeignPtr)
+import Foreign.Marshal.Utils (copyBytes)
+import Foreign.Ptr (Ptr, castPtr)
+import Foreign.Storable (Storable (..))
+import GHC.TypeNats (KnownNat, Nat, natVal)
 
 import HsBindgen.Runtime.Marshal (ReadRaw, StaticSize, WriteRaw)
+
+{-------------------------------------------------------------------------------
+  Definition
+-------------------------------------------------------------------------------}
 
 newtype ConstantArray (n :: Nat) a = CA (VS.Vector a)
   deriving (Eq, Show)
   deriving anyclass (ReadRaw, StaticSize, WriteRaw)
 
 type role ConstantArray nominal nominal
-
-repeat :: forall n a. (KnownNat n, Storable a) => a -> ConstantArray n a
-repeat x = CA (VS.replicate (intVal (Proxy @n)) x)
-
-toList :: Storable a => ConstantArray n a -> [a]
-toList (CA v) = VS.toList v
-
--- | Retrieve the underlying pointer
---
--- Coercible abstraction to look through the `newtype`s wrappers of typedefs.
---
-withPtr :: (Coercible b (ConstantArray n a), Storable a) => b -> (Ptr a -> IO a) -> IO a
-withPtr (coerce -> CA v) k = do
-    -- we copy the data, a e.g. int fun(int xs[3]) may mutate it.
-    VS.MVector _ fptr <- VS.thaw v
-    withForeignPtr fptr k
 
 instance (Storable a, KnownNat n) => Storable (ConstantArray n a) where
     sizeOf _ = intVal (Proxy @n) * sizeOf (undefined :: a)
@@ -61,6 +52,35 @@ instance (Storable a, KnownNat n) => Storable (ConstantArray n a) where
             copyBytes ptr (castPtr ptr') (size * sizeOfA)
       where
         sizeOfA = sizeOf (undefined :: a)
+
+{-------------------------------------------------------------------------------
+  Construction
+-------------------------------------------------------------------------------}
+
+repeat :: forall n a. (KnownNat n, Storable a) => a -> ConstantArray n a
+repeat x = CA (VS.replicate (intVal (Proxy @n)) x)
+
+{-------------------------------------------------------------------------------
+  Query
+-------------------------------------------------------------------------------}
+
+toList :: Storable a => ConstantArray n a -> [a]
+toList (CA v) = VS.toList v
+
+-- | Retrieve the underlying pointer
+--
+-- Coercible abstraction to look through the `newtype`s wrappers of typedefs.
+withPtr ::
+     (Coercible b (ConstantArray n a), Storable a)
+  => b -> (Ptr a -> IO a) -> IO a
+withPtr (coerce -> CA v) k = do
+    -- we copy the data, a e.g. int fun(int xs[3]) may mutate it.
+    VS.MVector _ fptr <- VS.thaw v
+    withForeignPtr fptr k
+
+{-------------------------------------------------------------------------------
+  Internal auxiliary
+-------------------------------------------------------------------------------}
 
 intVal :: forall n. KnownNat n => Proxy n -> Int
 intVal p = fromIntegral (natVal p)
