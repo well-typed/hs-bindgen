4c6c656a270b7107adceca432c0e96f4405ce3c1
Armando Santos <armandoifsantos@gmail.com>
2025-08-29 12:10:27 +0100

    Added decl location and main header to Hs.Comment
    - Also fixes #1017, by adding the original C Name to all
      declarations
    - Updates rendering of the file locations in order to fix #966

--- hs-bindgen/test/hs-bindgen/Test/HsBindgen/Orphans/TreeDiff.hs
+++ hs-bindgen/test/hs-bindgen/Test/HsBindgen/Orphans/TreeDiff.hs
@@ -136,7 +136,22 @@ instance ToExpr Hs.ExtHsRef
 instance ToExpr Hs.CallConv
 instance ToExpr Hs.ImportStyle
 
-instance ToExpr Hs.Comment
+-- | If there are unnamed structures in the parsed C header files, then
+-- 'Hs.Comment.commentOriin is going to point to an absolute path and line
+-- number where the said structure is defined. This absolute path is
+-- troublesome for golden tests because they won't run on the same machine who
+-- generated the fixtures. With this being said, we remove the commentCName
+-- from the picture.
+--
+-- Once #947 is done this won't be needed
+instance ToExpr Hs.Comment where
+  toExpr Hs.Comment{..} =
+    Expr.App "Comment" [ toExpr commentTitle
+                       , toExpr commentLocation
+                       , toExpr commentHeader
+                       , toExpr commentChildren
+                       ]
+
 instance ToExpr Hs.ListType
 instance ToExpr Hs.HeaderLevel
 instance ToExpr Hs.CommentBlockContent
