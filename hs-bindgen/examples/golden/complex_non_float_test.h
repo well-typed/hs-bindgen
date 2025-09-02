ed5ffefb725b054387e8f30a348c38d873f12197
Armando Santos <armandoifsantos@gmail.com>
2025-09-02 11:26:36 +0100

    Add complex_test golden test

---
+++ hs-bindgen/examples/golden/complex_non_float_test.h
@@ -0,0 +1,7 @@
+#include <complex.h>
+
+extern unsigned short  complex global_complex_unsigned_short;
+extern          short  complex global_complex_short;
+extern unsigned int    complex global_complex_unsigned_int;
+extern          int    complex global_complex_int;
+extern          char   complex global_complex_char;
