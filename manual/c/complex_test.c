a4371e6623fd4ee551bbfced20d583f361a605de
Armando Santos <armandoifsantos@gmail.com>
2025-09-05 17:20:26 +0100

    fx

--- manual/c/complex_test.c
+++ manual/c/complex_test.c
@@ -1,29 +1,4 @@
-#include <complex.h>
 #include "complex_test.h"
-
-unsigned short  complex global_complex_unsigned_short = 0;
-         short  complex global_complex_short          = 0;
-unsigned int    complex global_complex_unsigned_int   = 0;
-         int    complex global_complex_int            = 0;
-         char   complex global_complex_char           = 0;
-
-float  complex global_complex_float  = 1.0f + 2.0f * I;
-double complex global_complex_double = 3.0 + 4.0 * I;
-
-complex float  global_complex_float_flipped  = 5.0f + 6.0f * I;
-complex double global_complex_double_flipped = 7.0 + 8.0 * I;
-
-float  _Complex global_Complex_float  = 9.0f + 10.0f * I;
-double _Complex global_Complex_double = 11.0 + 12.0 * I;
-
-_Complex float  global_Complex_float_flipped  = 13.0f + 14.0f * I;
-_Complex double global_Complex_double_flipped = 15.0 + 16.0 * I;
-
-const float  complex const_complex_float  = 17.0f + 18.0f * I;
-const double complex const_complex_double = 19.0 + 20.0 * I;
-
-float  complex volatile_complex_float  = 21.0f + 22.0f * I;
-double complex volatile_complex_double = 23.0 + 24.0 * I;
 
 float  complex complex_float_array[10] = {
   1.0f + 1.0f * I, 2.0f + 2.0f * I, 3.0f + 3.0f * I, 4.0f + 4.0f * I, 5.0f  + 5.0f * I,
