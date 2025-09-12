bd4d1e6fb15020519bf1e3f26b701d3880a26ea2
Edsko de Vries <edsko@well-typed.com>
2025-09-12 08:06:18 +0200

    Avoid C warning

--- clang/cbits/rewrite_wrappers.h
+++ clang/cbits/rewrite_wrappers.h
@@ -7,7 +7,7 @@
 
 #include <clang-c/Rewrite.h>
 
-static inline void wrap_CXRewriter_insertTextBefore(CXRewriter Rew, CXSourceLocation *Loc, const char *Insert) {
+static inline void wrap_CXRewriter_insertTextBefore(CXRewriter Rew, const CXSourceLocation *Loc, const char *Insert) {
     clang_CXRewriter_insertTextBefore(Rew, *Loc, Insert);
 }
 
