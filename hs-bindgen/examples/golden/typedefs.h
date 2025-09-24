ea888d76be89e1701ed402ac30da04f302de2aea
Armando Santos <armandoifsantos@gmail.com>
2025-09-24 17:12:14 +0100

    Add function typedefs to golden tests

--- hs-bindgen/examples/golden/typedefs.h
+++ hs-bindgen/examples/golden/typedefs.h
@@ -4,3 +4,15 @@ typedef int * intptr;
 // Unsupported function with a typedef type. See issue #1034.
 typedef int int2int(int);
 extern int2int foo;
+
+typedef void (*FunctionPointer_Function)(void);
+typedef int (NonFunctionPointer_Function)(int value);
+
+typedef void (*f1)(void);
+
+typedef void g1(void);
+typedef g1 * g2;
+
+typedef void h1(void);
+typedef h1 h2;
+typedef h2 * h3;
