0b5d2d550cc4dcc91b91fd64c2207e2b79a4e441
Joris Dral <joris@well-typed.com>
2025-10-27 12:54:38 +0100

    `hs-bindgen-runtime`: add TODOs for testing arrays directly

---
+++ hs-bindgen-runtime/test/Test/HsBindgen/Runtime/IncompleteArray.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module Test.HsBindgen.Runtime.IncompleteArray (tests) where
+
+import Test.Tasty
+
+tests :: TestTree
+tests = testGroup "HsBindgen.Runtime.IncompleteArray" [
+    ]
+
+-- TODO: add tests, see issue #1222.
