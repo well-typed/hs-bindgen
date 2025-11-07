84d5e3ccc41149fed8465eb8611f8a6b6afc4aee
Armando Santos <armandoifsantos@gmail.com>
2025-11-07 12:12:42 +0000

    Add process exit code tests

--- hs-bindgen/test/hs-bindgen/Test/HsBindgen/Integration/ExitCode.hs
+++ hs-bindgen/test/hs-bindgen/Test/HsBindgen/Integration/ExitCode.hs
@@ -12,6 +12,9 @@ import Test.Tasty.HUnit
 import HsBindgen.Artefact (Artefact (..), NP (..))
 import HsBindgen.TraceMsg
 import HsBindgen.Util.Tracer
+import System.Process (readProcessWithExitCode)
+import System.Exit (ExitCode(..))
+import Debug.Trace qualified as Debug
 
 {-------------------------------------------------------------------------------
   Tests
@@ -21,25 +24,27 @@ tests :: IO TestResources -> TestTree
 tests testResources = testGroup "Integration.ExitCode" [
     testSuccessCase testResources
   , testUnresolvedInclude testResources
+  , testSuccessCaseProcess testResources
+  , testUnresolvedIncludeProcess testResources
   ]
 
 -- | Test that should successful complete without throwing TraceException
 --
 testSuccessCase :: IO TestResources -> TestTree
 testSuccessCase testResources = testCase "success does not throw" $ do
-    let test = defaultTest "simple_func"
-        artefacts = FinalDecls :* Nil
-        noReport = const $ pure ()
+  let test = defaultTest "simple_func"
+      artefacts = FinalDecls :* Nil
+      noReport = const $ pure ()
 
-    result <- try $ runTestHsBindgen' noReport testResources test artefacts
-    case result of
-      Right _ -> pure ()
-      Left e'
-        | Just (TraceException @TraceMsg _)
-          <- fromException e' ->
-            assertFailure "Expected success but got TraceException"
-        | otherwise ->
-          assertFailure $ "Unexpected exception: " ++ show e'
+  result <- try $ runTestHsBindgen' noReport testResources test artefacts
+  case result of
+    Right _ -> pure ()
+    Left e'
+      | Just (TraceException @TraceMsg _)
+        <- fromException e' ->
+          assertFailure "Expected success but got TraceException"
+      | otherwise ->
+        assertFailure $ "Unexpected exception: " ++ show e'
 
 -- | Test unresolved #include (issue #1197 scenario)
 --
@@ -47,24 +52,57 @@ testSuccessCase testResources = testCase "success does not throw" $ do
 --
 testUnresolvedInclude :: IO TestResources -> TestTree
 testUnresolvedInclude testResources = testCase "unresolved include throws TraceException" $ do
-    withSystemTempDirectory "hs-bindgen-test" $ \tmpDir -> do
-      let tempHeader = tmpDir </> "test-unresolved.h"
-      writeFile tempHeader "#include <nonexistent/totally-bogus-header-12345.h>\n"
+  withSystemTempDirectory "hs-bindgen-test" $ \tmpDir -> do
+    let tempHeader = tmpDir </> "test-unresolved.h"
+    writeFile tempHeader "#include <nonexistent/totally-bogus-header-12345.h>\n"
 
-      let test = (defaultFailingTest "test-unresolved") {
-              testDir = tmpDir
-              -- Tolerate all traces - we want to test TraceException propagation
-            , testTracePredicate = customTracePredicate [] $ \_ -> Just Tolerated
-            }
-          artefacts = FinalDecls :* Nil
-          noReport = const $ pure ()
+    let test = (defaultFailingTest "test-unresolved") {
+            testDir = tmpDir
+            -- Tolerate all traces - we want to test TraceException propagation
+          , testTracePredicate = customTracePredicate [] $ \_ -> Just Tolerated
+          }
+        artefacts = FinalDecls :* Nil
+        noReport = const $ pure ()
 
-      result <- try $ runTestHsBindgen' noReport testResources test artefacts
-      case result of
-        Left e'
-          | Just (TraceException @TraceMsg _)
-            <- fromException e' -> pure ()
-          | otherwise ->
-              assertFailure $ "Expected TraceException but got: " ++ show e'
-        Right _ ->
-          assertFailure "Expected TraceException for unresolved include but got success"
+    result <- try $ runTestHsBindgen' noReport testResources test artefacts
+    case result of
+      Left e'
+        | Just (TraceException @TraceMsg _)
+          <- fromException e' -> pure ()
+        | otherwise ->
+            assertFailure $ "Expected TraceException but got: " ++ show e'
+      Right _ ->
+        assertFailure "Expected TraceException for unresolved include but got success"
+
+-- | Test that actual process exit code
+--
+testSuccessCaseProcess :: IO TestResources -> TestTree
+testSuccessCaseProcess testResources = testCase "success returns exit code 0" $ do
+  withSystemTempDirectory "hs-bindgen-test" $ \tmpDir -> do
+    root <- getTestPackageRoot testResources
+    let headerPath = root </> "examples/golden/simple_func.h"
+    (exitCode, _, _) <- readProcessWithExitCode "hs-bindgen-cli"
+                                               [ "preprocess"
+                                               , "--hs-output-dir"
+                                               , tmpDir
+                                               , headerPath
+                                               ]
+                                               ""
+    exitCode @?= ExitSuccess
+
+-- Test unresolved #include (the original issue scenario)
+testUnresolvedIncludeProcess :: IO TestResources -> TestTree
+testUnresolvedIncludeProcess testResources = testCase "unresolved include returns non-zero" $ do
+  withSystemTempDirectory "hs-bindgen-test" $ \tmpDir -> do
+    root <- getTestPackageRoot testResources
+    -- Create a temporary header with unresolved include
+    let tempHeader = root </> "test-unresolved-include.h"
+    writeFile tempHeader "#include <nonexistent/totally-bogus-header-12345.h>\n"
+    (exitCode, _, _) <- readProcessWithExitCode "hs-bindgen-cli"
+                                               [ "preprocess"
+                                               , "--hs-output-dir"
+                                               , tmpDir
+                                               , tempHeader
+                                               ]
+                                               ""
+    exitCode @?= ExitFailure 1
