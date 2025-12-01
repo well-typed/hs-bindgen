4a2d1d93f184d039f586658c8dc60ecded3f5cb8
Joris Dral <joris@well-typed.com>
2025-12-01 15:34:09 +0100

    Put `InstanceMap` in its own module

---
+++ hs-bindgen/src-internal/HsBindgen/Backend/Hs/Translation/Instances.hs
@@ -0,0 +1,174 @@
+-- | Type classes, instances, and constraints
+module HsBindgen.Backend.Hs.Translation.Instances (
+    InstanceMap
+  , getInstances
+  ) where
+
+import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
+
+import HsBindgen.Backend.Hs.AST
+import HsBindgen.Backend.Hs.AST.Type
+import HsBindgen.BindingSpec
+import HsBindgen.Errors
+import HsBindgen.Imports
+import HsBindgen.Language.Haskell
+
+type InstanceMap = Map (Name NsTypeConstr) (Set TypeClass)
+
+getInstances ::
+     HasCallStack
+  => InstanceMap       -- ^ Current state
+  -> Name NsTypeConstr -- ^ Name of current type
+  -> Set TypeClass     -- ^ Candidate instances
+  -> [HsType]          -- ^ Dependencies
+  -> Set TypeClass
+getInstances instanceMap name = aux
+  where
+    aux :: Set TypeClass -> [HsType] -> Set TypeClass
+    aux acc [] = acc
+    aux acc (hsType:hsTypes)
+      | Set.null acc = acc
+      | otherwise = case hsType of
+          HsPrimType primType -> aux (acc /\ hsPrimTypeInsts primType) hsTypes
+          HsTypRef name'
+            | name' == name -> aux acc hsTypes
+            | otherwise -> case Map.lookup name' instanceMap of
+                Just instances -> aux (acc /\ instances) hsTypes
+                Nothing -> panicPure $ "type not found: " ++ show name'
+          HsConstArray _n hsType' ->
+            -- constrain by ConstantArray item type in next step
+            aux (acc /\ cArrayInsts) $ hsType' : hsTypes
+          HsIncompleteArray hsType' ->
+            -- constrain by Array item type in next step
+            aux (acc /\ arrayInsts) $ hsType' : hsTypes
+          HsPtr{} -> aux (acc /\ ptrInsts) hsTypes
+          HsFunPtr{} -> aux (acc /\ ptrInsts) hsTypes
+          HsIO{} -> Set.empty
+          HsFun{} -> Set.empty
+          HsExtBinding _ref _cTypeSpec mHsTypeSpec ->
+            let acc' = case mHsTypeSpec of
+                  Just hsTypeSpec -> acc /\ hsTypeSpecInsts hsTypeSpec
+                  Nothing         -> acc
+            in  aux acc' hsTypes
+          HsByteArray{} ->
+            let acc' = acc /\ Set.fromList [Eq, Ord, Show]
+            in  aux acc' hsTypes
+          HsSizedByteArray{} ->
+            let acc' = acc /\ Set.fromList [Eq, Show]
+            in  aux acc' hsTypes
+          HsBlock t ->
+            aux acc (t:hsTypes)
+          HsComplexType primType -> aux (acc /\ hsPrimTypeInsts primType) hsTypes
+          HsStrLit{} -> Set.empty
+
+    (/\) :: Ord a => Set a -> Set a -> Set a
+    (/\) = Set.intersection
+
+    hsPrimTypeInsts :: HsPrimType -> Set TypeClass
+    hsPrimTypeInsts = \case
+      HsPrimVoid       -> Set.fromList [Eq, Ix, Ord, Read, Show]
+      HsPrimUnit       -> unitInsts
+      HsPrimCChar      -> integralInsts
+      HsPrimCSChar     -> integralInsts
+      HsPrimCUChar     -> integralInsts
+      HsPrimCInt       -> integralInsts
+      HsPrimCUInt      -> integralInsts
+      HsPrimCShort     -> integralInsts
+      HsPrimCUShort    -> integralInsts
+      HsPrimCLong      -> integralInsts
+      HsPrimCULong     -> integralInsts
+      HsPrimCPtrDiff   -> integralInsts
+      HsPrimCSize      -> integralInsts
+      HsPrimCLLong     -> integralInsts
+      HsPrimCULLong    -> integralInsts
+      HsPrimCBool      -> integralInsts
+      HsPrimCFloat     -> floatingInsts
+      HsPrimCDouble    -> floatingInsts
+      HsPrimCStringLen -> Set.fromList [Eq, Ord, Show]
+      HsPrimInt        -> integralInsts
+
+    unitInsts :: Set TypeClass
+    unitInsts = Set.fromList [
+        Eq
+      , Ord
+      , Read
+      , ReadRaw
+      , Show
+      , StaticSize
+      , Storable
+      , WriteRaw
+      ]
+
+    integralInsts :: Set TypeClass
+    integralInsts = Set.fromList [
+        Bits
+      , Bounded
+      , Enum
+      , Eq
+      , FiniteBits
+      , Integral
+      , Ix
+      , Num
+      , Ord
+      , Read
+      , ReadRaw
+      , Real
+      , Show
+      , StaticSize
+      , Storable
+      , WriteRaw
+      ]
+
+    floatingInsts :: Set TypeClass
+    floatingInsts = Set.fromList [
+        Enum
+      , Eq
+      , Floating
+      , Fractional
+      , Num
+      , Ord
+      , Read
+      , ReadRaw
+      , Real
+      , RealFloat
+      , RealFrac
+      , Show
+      , StaticSize
+      , Storable
+      , WriteRaw
+      ]
+
+    ptrInsts :: Set TypeClass
+    ptrInsts = Set.fromList [
+        Eq
+      , Ord
+      , ReadRaw
+      , Show
+      , StaticSize
+      , Storable
+      , WriteRaw
+      ]
+
+    cArrayInsts :: Set TypeClass
+    cArrayInsts = Set.fromList [
+        Eq
+      , ReadRaw
+      , Show
+      , StaticSize
+      , Storable
+      , WriteRaw
+      ]
+
+    arrayInsts :: Set TypeClass
+    arrayInsts = Set.fromList [
+        Eq
+      , Show
+      ]
+
+    hsTypeSpecInsts :: HsTypeSpec -> Set TypeClass
+    hsTypeSpecInsts hsTypeSpec = Set.fromAscList [
+        cls
+      | (cls, Require{}) <-
+           Map.toAscList (hsTypeSpecInstances hsTypeSpec)
+      ]
