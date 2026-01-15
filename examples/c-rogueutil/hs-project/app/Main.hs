0d1f758882757516ffa416f8c71f65672035ae47
Armando Santos <armandoifsantos@gmail.com>
2026-01-15 17:19:15 +0000

    Make c-rogueutil work with Category selection flags

--- examples/c-rogueutil/hs-project/app/Main.hs
+++ examples/c-rogueutil/hs-project/app/Main.hs
@@ -6,41 +6,40 @@ import Control.Monad (forM_)
 import Foreign.C (newCString, withCString)
 
 import RogueUtil.Generated qualified as RU
-import RogueUtil.Generated.Safe qualified as Safe
 
 import HsBindgen.Runtime.ConstPtr qualified as HBR
 
 main :: IO ()
 main = do
-  _ <- Safe.saveDefaultColor
+  _ <- RU.saveDefaultColor
 
-  Safe.cls
-  Safe.hidecursor
+  RU.cls
+  RU.hidecursor
 
-  Safe.setColor (fromIntegral $ RU.un_Color_code RU.YELLOW)
-  Safe.setBackgroundColor (fromIntegral $ RU.un_Color_code RU.BLUE)
+  RU.setColor (fromIntegral $ RU.un_Color_code RU.YELLOW)
+  RU.setBackgroundColor (fromIntegral $ RU.un_Color_code RU.BLUE)
   titleStr <- newCString "=== RogueUtil Haskell Bindings Demo ==="
-  Safe.printXY 20 2 (RU.RUTIL_STRING $ HBR.ConstPtr titleStr)
-  Safe.resetColor
+  RU.printXY 20 2 (RU.RUTIL_STRING $ HBR.ConstPtr titleStr)
+  RU.resetColor
 
-  rows <- Safe.trows
-  cols <- Safe.tcols
-  Safe.setColor (fromIntegral $ RU.un_Color_code RU.CYAN)
+  rows <- RU.trows
+  cols <- RU.tcols
+  RU.setColor (fromIntegral $ RU.un_Color_code RU.CYAN)
   sizeMsg <- newCString $ "Terminal size: " ++ show cols ++ "x" ++ show rows
-  Safe.printXY 20 4 (RU.RUTIL_STRING $ HBR.ConstPtr sizeMsg)
-  Safe.resetColor
+  RU.printXY 20 4 (RU.RUTIL_STRING $ HBR.ConstPtr sizeMsg)
+  RU.resetColor
 
   let colors = [RU.RED, RU.YELLOW, RU.GREEN, RU.CYAN, RU.BLUE, RU.MAGENTA]
   forM_ (zip [0 :: Int ..] colors) $ \(i, color) -> do
-    Safe.setColor (fromIntegral $ RU.un_Color_code color)
+    RU.setColor (fromIntegral $ RU.un_Color_code color)
     withCString ("█████ " ++ show color) $ \str ->
-      Safe.printXY 20 (6 + fromIntegral i) (RU.RUTIL_STRING $ HBR.ConstPtr str)
-  Safe.resetColor
+      RU.printXY 20 (6 + fromIntegral i) (RU.RUTIL_STRING $ HBR.ConstPtr str)
+  RU.resetColor
 
-  Safe.locate 20 14
-  Safe.showcursor
+  RU.locate 20 14
+  RU.showcursor
   pressKeyMsg <- newCString "Press any key to exit..."
-  Safe.anykey (RU.RUTIL_STRING $ HBR.ConstPtr pressKeyMsg)
+  RU.anykey (RU.RUTIL_STRING $ HBR.ConstPtr pressKeyMsg)
 
-  Safe.cls
-  Safe.resetColor
+  RU.cls
+  RU.resetColor
