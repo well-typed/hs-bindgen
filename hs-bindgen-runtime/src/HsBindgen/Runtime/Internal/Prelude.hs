83fd965d942a3c370e7b17972b1f56d9c021da83
Dominik Schrempf <dominik@well-typed.com>
2026-01-29 09:40:54 +0100

    HsBindgen.Runtime.Internal.Prelude stub with documentation

---
+++ hs-bindgen-runtime/src/HsBindgen/Runtime/Internal/Prelude.hs
@@ -0,0 +1,10 @@
+-- | Prelude required by generated bindings.
+--
+-- This module ensures compatibility across GHC versions and @base@ library
+-- versions. It re-exports most Haskell definitions that are used by the
+-- generated bindings.
+--
+-- See https://github.com/well-typed/hs-bindgen/issues/1627.
+module HsBindgen.Runtime.Internal.Prelude (
+
+  ) where
