723be7a1daf6c143955d403bbc5ffc8ab0006dcb
Dominik Schrempf <dominik@well-typed.com>
2026-01-30 10:46:32 +0100

    Runtime: Rename CBoolSem to CBool

--- hs-bindgen-runtime/test/test-runtime.hs
+++ hs-bindgen-runtime/test/test-runtime.hs
@@ -3,7 +3,7 @@ module Main (main) where
 import Test.Tasty (defaultMain, testGroup)
 
 import Test.HsBindgen.Runtime.Bitfield qualified
-import Test.HsBindgen.Runtime.CBoolSem qualified
+import Test.HsBindgen.Runtime.CBool qualified
 import Test.HsBindgen.Runtime.CEnum qualified
 import Test.HsBindgen.Runtime.ConstantArray qualified
 import Test.HsBindgen.Runtime.IncompleteArray qualified
@@ -16,7 +16,7 @@ import Test.HsBindgen.Runtime.SizedByteArray qualified
 main :: IO ()
 main = defaultMain $ testGroup "test-runtime" [
       Test.HsBindgen.Runtime.Bitfield.tests
-    , Test.HsBindgen.Runtime.CBoolSem.tests
+    , Test.HsBindgen.Runtime.CBool.tests
     , Test.HsBindgen.Runtime.CEnum.tests
     , Test.HsBindgen.Runtime.ConstantArray.tests
     , Test.HsBindgen.Runtime.IncompleteArray.tests
