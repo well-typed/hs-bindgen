292ea3017930263f9512e636c28370fe29494855
Joris Dral <joris@well-typed.com>
2026-03-03 15:49:13 +0100

    Manual section on zero-copy API

--- manual/LowLevel/Translation/05-ZeroCopy.md
+++ manual/LowLevel/Translation/05-ZeroCopy.md
@@ -1,3 +1,46 @@
-# Zero-copy bindings
+# Zero-copy API
 
-TODO: write text. See issue #1288.
+The zero-copy API allows C objects to be manipulated through pointers without
+marshalling the entire object to Haskell and then back to C. The term *zero-copy
+API* colloquially refers to both the code that `hs-bindgen` generates to do this
+type of pointer manipulation, and also the infrastructure in
+`hs-bindgen-runtime` that supports it.
+
+## Introduction
+
+Suppose we want to override one value deeply nested in some C data structure. We
+could use the `Storable` instances to `peek` the value, then override the
+appropriate field, and finally `poke` the updated value:
+
+```hs
+introduction :: IO ()
+introduction =
+    alloca $ \(rectPtr :: Ptr Gen.Rectangle) -> do
+      rect <- peek rectPtr
+      poke rectPtr $ rect & #rectangle_topleft % #point_x .~ 2
+```
+
+(The use of lenses here is optional of course.)
+
+However, it may well be undesirable to marshall the entire structure back and
+forth merely to change a single value. This is why `hs-bindgen` also generates
+`HasField` instances for pointers, so that record dot syntax can be used to
+index C structures. We can update the same field as before without marshalling
+the entire structure as follows:
+
+```hs
+      poke rectPtr.rectangle_topleft.point_x 2
+```
+
+`HasField` instances are implemented under the hood with a custom class
+infrastructure. If you prefer to avoid record-dot syntax, you can use the
+``HsBindgen.Runtime.HasCField` infrastructure directly.
+
+The zero-copy API also supports zero-copy pointer manipulation of C objects
+other than just structures. The supported operations are:
+
+* Indexing of union fields through a pointer
+* Indexing of bit-fields (for both structures and unions) through a pointer
+* Erasing `newtype` sugar (introduced by `typedef`s or macro-defined types) from
+  a pointer type
+* Convering a pointer to an array to a pointer to the array's first element
--- manual/hs/manual/app/Manual/ZeroCopy.hs
+++ manual/hs/manual/app/Manual/ZeroCopy.hs
@@ -1,15 +1,14 @@
+{-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE ViewPatterns #-}
-{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Manual.ZeroCopy (examples) where
 
 import Foreign
-import Foreign.C.Types
+import Optics
 
 import HsBindgen.Runtime.BitfieldPtr qualified as BitfieldPtr
 import HsBindgen.Runtime.ConstantArray qualified as CA
-import HsBindgen.Runtime.FLAM qualified as FLAM
 import HsBindgen.Runtime.Prelude
 import HsBindgen.Runtime.PtrConst qualified as PtrConst
 
@@ -21,104 +20,56 @@ examples :: IO ()
 examples = do
     section "Zero-copy bindings"
     do
+      subsection "Introduction"
+      introduction
+
       subsection "Structs"
-      do
-        let rect = Gen.Rectangle (Gen.Point 1 2) (Gen.Point 3 4)
-        with rect $ \ptr -> do
-          peek ptr >>= print
-          scaleRectangle 2 ptr
-          peek ptr >>= print
+      structs
 
       subsection "Unions"
-      do
-        let rect = Gen.Rectangle (Gen.Point 1 1) (Gen.Point 3 3)
-            shape = Gen.set_shape_rectangle rect
-        with shape $ \ptr -> do
-          peek ptr.shape_rectangle >>= print
-          poke ptr.shape_circle.circle_midpoint.point_x 5
-          poke ptr.shape_circle.circle_midpoint.point_y 5
-          poke ptr.shape_circle.circle_radius 2
-          peek ptr.shape_circle >>= print
+      unions
 
       subsection "Bit-fields"
-      do
-        let colour = Gen.Colour 0 6 127 255 127
-        with colour $ \ptr -> do
-          peek ptr >>= print
-          uptickBrightness ptr
-          peek ptr >>= print
-          uptickBrightness ptr -- overflow!
-          peek ptr >>= print
+      bitFields
 
       subsection "Typedefs"
-      do
-        with (Gen.MyInt 17) $ \ptr -> do
-          peek ptr >>= print
-          poke ptr (Gen.MyInt 18)
-          peek ptr >>= print
-          poke ptr.unwrapMyInt 19
-          peek ptr >>= print
-          poke ptr (Gen.MyInt 20)
-          peek ptr.unwrapMyInt >>= print
+      typedefs
 
       subsection "Pointers"
-      do
-        let shape  = Gen.set_shape_rectangle
-                       (Gen.Rectangle (Gen.Point 0 0) (Gen.Point 10 1))
-            colour = Gen.Colour 1 4 0 255 0
-        withDrawing shape colour $ \drawing ->
-          with drawing $ \ptr -> do
-            peek ptr >>= print
-            shapePtr <- peek ptr.drawing_shape
-            peek shapePtr.shape_rectangle >>= print
-            scaleRectangle 10 shapePtr.shape_rectangle
-            peek shapePtr.shape_rectangle >>= print
-            peek ptr >>= print
+      pointers
 
       subsection "Arrays"
-      do
-        let ticTacToe = Gen.Tic_tac_toe
-              (CA.fromList $ [0, 0, 0])
-              (CA.fromList $ [0, 0, 0])
-              (CA.fromList $ [0, 0, 0])
-        with ticTacToe $ \ptr -> do
-          peek ptr >>= print
-          pokeElemOff ptr.tic_tac_toe_row1.toFirstElemPtr 2 1
-          peek ptr >>= print
-          pokeElemOff ptr.tic_tac_toe_row2.toFirstElemPtr 1 2
-          peek ptr >>= print
-          pokeElemOff (ptr.tic_tac_toe_row2.toFirstElemPtr) 2 1
-          peek ptr >>= print
+      arrays
 
       subsection "Flexible array members"
-      do
-        -- TODO: add a section on zero-copy bindings for flexible array members.
-        -- See issue #1286.
-        print "TODO"
+      flexibleArrayMembers
 
       subsection "Multi-dimensional arrays (and typedefs)"
-      do
-        let matrix = Gen.Matrix $ CA.fromList [
-                Gen.Triplet $ CA.fromList [1, 2, 3]
-              , Gen.Triplet $ CA.fromList [4, 5, 6]
-              , Gen.Triplet $ CA.fromList [7, 8, 9]
-              ]
-        with matrix $ \ptr1 ->
-          alloca $ \ptr2 -> do
-            peek ptr1 >>= print
+      multiDimensionalArrays
 
-            transpose ptr1 ptr2
-            peek ptr2 >>= print
+{-------------------------------------------------------------------------------
+  Introduction
+-------------------------------------------------------------------------------}
 
-            pokeElemOff
-              (ptr2.unwrapMatrix.toFirstElemPtr `plusPtrElem` 2
-              ).unwrapTriplet.toFirstElemPtr
-              1
-              0
-            peek ptr2 >>= print
+introduction :: IO ()
+introduction =
+    alloca $ \(rectPtr :: Ptr Gen.Rectangle) -> do
+      rect <- peek rectPtr
+      poke rectPtr $ rect & #rectangle_topleft % #point_x .~ 2
 
-            transpose ptr2 ptr1
-            peek ptr1 >>= print
+      poke rectPtr.rectangle_topleft.point_x 2
+
+{-------------------------------------------------------------------------------
+  Structs
+-------------------------------------------------------------------------------}
+
+structs :: IO ()
+structs = do
+    let rect = Gen.Rectangle (Gen.Point 1 2) (Gen.Point 3 4)
+    with rect $ \ptr -> do
+      peek ptr >>= print
+      scaleRectangle 2 ptr
+      peek ptr >>= print
 
 scaleRectangle :: Int -> Ptr Gen.Rectangle -> IO ()
 scaleRectangle (fromIntegral -> c) ptr = do
@@ -134,10 +85,73 @@ scaleRectangle (fromIntegral -> c) ptr = do
     y2 <- peek ptr.rectangle_bottomright.point_y
     poke ptr.rectangle_bottomright.point_y (y2*c)
 
+{-------------------------------------------------------------------------------
+  Unions
+-------------------------------------------------------------------------------}
+
+unions :: IO ()
+unions = do
+    let rect = Gen.Rectangle (Gen.Point 1 1) (Gen.Point 3 3)
+        shape = Gen.set_shape_rectangle rect
+    with shape $ \ptr -> do
+      peek ptr.shape_rectangle >>= print
+      poke ptr.shape_circle.circle_midpoint.point_x 5
+      poke ptr.shape_circle.circle_midpoint.point_y 5
+      poke ptr.shape_circle.circle_radius 2
+      peek ptr.shape_circle >>= print
+
+{-------------------------------------------------------------------------------
+  Bit-fields
+-------------------------------------------------------------------------------}
+
+bitFields :: IO ()
+bitFields = do
+    let colour = Gen.Colour 0 6 127 255 127
+    with colour $ \ptr -> do
+      peek ptr >>= print
+      uptickBrightness ptr
+      peek ptr >>= print
+      uptickBrightness ptr -- overflow!
+      peek ptr >>= print
+
+
 uptickBrightness :: Ptr Gen.Colour -> IO ()
 uptickBrightness ptr = do
     brightness <- BitfieldPtr.peek ptr.colour_brightness
     BitfieldPtr.poke ptr.colour_brightness (brightness+1)
+
+{-------------------------------------------------------------------------------
+  Typedefs
+-------------------------------------------------------------------------------}
+
+typedefs :: IO ()
+typedefs =
+    with (Gen.MyInt 17) $ \ptr -> do
+      peek ptr >>= print
+      poke ptr (Gen.MyInt 18)
+      peek ptr >>= print
+      poke ptr.unwrapMyInt 19
+      peek ptr >>= print
+      poke ptr (Gen.MyInt 20)
+      peek ptr.unwrapMyInt >>= print
+
+{-------------------------------------------------------------------------------
+  Pointers
+-------------------------------------------------------------------------------}
+
+pointers :: IO ()
+pointers = do
+    let shape  = Gen.set_shape_rectangle
+                    (Gen.Rectangle (Gen.Point 0 0) (Gen.Point 10 1))
+        colour = Gen.Colour 1 4 0 255 0
+    withDrawing shape colour $ \drawing ->
+      with drawing $ \ptr -> do
+        peek ptr >>= print
+        shapePtr <- peek ptr.drawing_shape
+        peek shapePtr.shape_rectangle >>= print
+        scaleRectangle 10 shapePtr.shape_rectangle
+        peek shapePtr.shape_rectangle >>= print
+        peek ptr >>= print
 
 withDrawing :: Gen.Shape -> Gen.Colour -> (Gen.Drawing -> IO a) -> IO a
 withDrawing shape colour k =
@@ -145,11 +159,65 @@ withDrawing shape colour k =
     with colour $ \colourPtr ->
     k (Gen.Drawing shapePtr colourPtr)
 
+{-------------------------------------------------------------------------------
+  Arrays
+-------------------------------------------------------------------------------}
+
+arrays :: IO ()
+arrays = do
+    let ticTacToe = Gen.Tic_tac_toe
+          (CA.fromList $ [0, 0, 0])
+          (CA.fromList $ [0, 0, 0])
+          (CA.fromList $ [0, 0, 0])
+    with ticTacToe $ \ptr -> do
+      peek ptr >>= print
+      pokeElemOff ptr.tic_tac_toe_row1.toFirstElemPtr 2 1
+      peek ptr >>= print
+      pokeElemOff ptr.tic_tac_toe_row2.toFirstElemPtr 1 2
+      peek ptr >>= print
+      pokeElemOff (ptr.tic_tac_toe_row2.toFirstElemPtr) 2 1
+      peek ptr >>= print
+
+{-------------------------------------------------------------------------------
+  Flexible array members
+-------------------------------------------------------------------------------}
+
+flexibleArrayMembers :: IO ()
+flexibleArrayMembers = do
+    -- TODO: add a section on zero-copy bindings for flexible array members.
+    -- See issue #1286.
+    print "TODO"
+
+{-------------------------------------------------------------------------------
+  Multi-dimensional arrays (and typedefs)
+-------------------------------------------------------------------------------}
+
+multiDimensionalArrays :: IO ()
+multiDimensionalArrays = do
+    let matrix = Gen.Matrix $ CA.fromList [
+            Gen.Triplet $ CA.fromList [1, 2, 3]
+          , Gen.Triplet $ CA.fromList [4, 5, 6]
+          , Gen.Triplet $ CA.fromList [7, 8, 9]
+          ]
+    with matrix $ \ptr1 ->
+      alloca $ \ptr2 -> do
+        peek ptr1 >>= print
+
+        transpose ptr1 ptr2
+        peek ptr2 >>= print
+
+        pokeElemOff
+          (ptr2.unwrapMatrix.toFirstElemPtr `plusPtrElem` 2
+          ).unwrapTriplet.toFirstElemPtr
+          1
+          0
+        peek ptr2 >>= print
+
+        transpose ptr2 ptr1
+        peek ptr1 >>= print
+
 transpose :: Ptr Gen.Matrix -> Ptr Gen.Matrix -> IO ()
 transpose inputPtr outputPtr =
     Gen.transpose
       (PtrConst.unsafeFromPtr inputPtr.unwrapMatrix.toFirstElemPtr)
       outputPtr.unwrapMatrix.toFirstElemPtr
-
-instance FLAM.NumElems CChar Gen.Vector_Aux where
-  numElems x = fromIntegral (Gen.vector_len x)
