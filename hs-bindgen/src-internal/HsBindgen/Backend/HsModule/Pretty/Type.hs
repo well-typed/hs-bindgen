17ff8331b706fa560ce4f22e94f8fc3abcda1bc8
Joris Dral <joris@well-typed.com>
2026-04-17 16:56:22 +0200

    `SHs`: documentation updates for type pretty-printing

--- hs-bindgen/src-internal/HsBindgen/Backend/HsModule/Pretty/Type.hs
+++ hs-bindgen/src-internal/HsBindgen/Backend/HsModule/Pretty/Type.hs
@@ -6,9 +6,10 @@ module HsBindgen.Backend.HsModule.Pretty.Type (
     -- * Precedences
   , funPrec
   , funPrec1
+  , eqPrec
+  , eqPrec1
   , appPrec
   , appPrec1
-  , maxPrec
   ) where
 
 import DeBruijn (Add (..), EmptyCtx, Env (..), lookupEnv)
@@ -36,25 +37,35 @@ prettyType env prec ty = case ty of
       TLit n -> PP.show n
       TStrLit s -> PP.string (show s)
       TExt i _cTypeSpec _hsTypeSpec -> pretty i
-      TApps (TBoxedTup n) args ->
-        let decls = prettyType env 0 <$> args
-        in  prettyBoxedTuple n decls
-      TApps TEq [l, r] ->
-        prettyType env eqPrec1 l <+> PP.string "~" <+> prettyType env eqPrec1 r
-      TApp c x -> PP.parensWhen (prec > appPrec) $
-        prettyType env appPrec c <+> prettyType env appPrec1 x
       TFun a b -> PP.parensWhen (prec > funPrec) $
         prettyType env funPrec1 a <+> "->" <+> prettyType env funPrec b
       TBound x -> lookupEnv x env
       TUnit -> PP.string "()"
-      TBoxedTup n -> prettyBoxedTuple n []
-      TEq -> PP.string "(~)"
       TForall hints add ctxt body ->
         case add of
           AZ -> PP.hsep (map (\ ct -> prettyType env 0 ct <+> "=> ") ctxt) >< prettyType env 0 body
           _  -> withFreshNames env add hints $ \env' params ->
             "forall" <+> PP.hsep params >< "." <+>
             PP.hsep (map (\ ct -> prettyType env' 0 ct <+> "=>") ctxt) <+> prettyType env' 0 body
+
+      --We print tuples without sections if we can
+      TApps (TBoxedTup n) args ->
+        let decls = prettyType env 0 <$> args
+        in  prettyBoxedTuple n decls
+      TBoxedTup n -> prettyBoxedTuple n []
+
+      -- When the type operator is fully applied, we print it with infix
+      -- notation. If it is not fully applied (or is applied to too many types),
+      -- we print the operator with prefix notation. Sections are not supported
+      -- for type operators.
+      TApps TEq [l, r] ->
+        prettyType env eqPrec1 l <+> PP.string "~" <+> prettyType env eqPrec1 r
+      TEq -> PP.string "(~)"
+
+      -- This case should be last, so that TApps cases and their alternatives
+      -- are matched on first
+      TApp c x -> PP.parensWhen (prec > appPrec) $
+        prettyType env appPrec c <+> prettyType env appPrec1 x
 
 {-------------------------------------------------------------------------------
   Precedences
@@ -71,6 +82,3 @@ eqPrec1 = eqPrec + 1
 appPrec, appPrec1:: Int
 appPrec = 10
 appPrec1 = appPrec + 1
-
-maxPrec :: Int
-maxPrec = 20
