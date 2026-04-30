5a2d1c63c122028f8a4dbb404b49f87bd2537599
Armando Santos <armando@well-typed.com>
2026-04-30 11:57:15 +0100

    Move doxygen-parser to its own repo

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:well-typed/libclang?rev=cdbf817187a261ebf8c31b32c85c5693eedf298d";
       flake = false;
     };
+    doxygen-parser-src = {
+      url = "github:well-typed/doxygen-parser?rev=64faaf3d43599076eb5a04eae7af777de74c7e9b";
+      flake = false;
+    };
   };
 
   outputs =
@@ -16,12 +20,13 @@
       nixpkgs,
       #
       libclang-bindings-src,
+      doxygen-parser-src,
       ...
     }:
     let
       overlays = import ./nix/overlay {
         inherit (nixpkgs) lib;
-        inherit libclang-bindings-src;
+        inherit libclang-bindings-src doxygen-parser-src;
       };
     in
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -32,7 +37,7 @@
         "x86_64-darwin"
       ];
       perSystem = import ./nix/hs-bindgen.nix {
-        inherit nixpkgs libclang-bindings-src;
+        inherit nixpkgs libclang-bindings-src doxygen-parser-src;
       };
       flake.overlays = overlays;
     };
