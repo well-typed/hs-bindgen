7a4326e1bef4c955278bccccca17e501fc0d17ff
Joris Dral <joris@well-typed.com>
2026-07-23 14:12:10 +0200

    `Golden`: naming implicit fields after external types cause name conflicts

---
+++ hs-bindgen/test-artefacts/headers/golden/types/anonymous/edge-cases/name_conflict.h
@@ -0,0 +1,11 @@
+struct Rectangle {
+  struct { // Point2D
+      int x1; // top-left X coordinate
+      int y1; // top-left Y coordinate
+    };
+  struct { // Point3D
+    int x2; // bottom-right X coordinate
+    int y2; // bottom-right Y coordinate
+  };
+};
+
