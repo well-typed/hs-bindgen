2571df247e1af027033341d8337eb88f6ab4c440
KovalevDima <onarainydayillwinanyway@gmail.com>
2026-08-23 19:22:29 +0000

    Fix hs-bindgen nix build

--- nix/generated/libclang-bindings.nix
+++ nix/generated/libclang-bindings.nix
@@ -1,12 +1,18 @@
 { mkDerivation, base, bytestring, containers, data-default
-, directory, exceptions, filepath, lib, mtl, process, QuickCheck
-, tasty, tasty-hunit, tasty-quickcheck, template-haskell, text
-, transformers, unliftio-core
+, directory, exceptions, fetchgit, filepath, lib, mtl, process
+, QuickCheck, tasty, tasty-hunit, tasty-quickcheck
+, template-haskell, text, transformers, unliftio-core
 }:
 mkDerivation {
   pname = "libclang-bindings";
   version = "0.1.0.0";
-  sha256 = "3c4b9c61c78c99b4cf1362cdf18247f933f6493ce23ace23ffec8132ee998db6";
+  src = fetchgit {
+    url = "https://github.com/well-typed/libclang-bindings";
+    sha256 = "10xxy0qixhmzincsl172h65fal3zlyvnl9035ip45l1j4prhpvqy";
+    rev = "c1340ddbd9b26cd59f143f7625beb48187a863bf";
+    fetchSubmodules = true;
+  };
+  postUnpack = "sourceRoot+=/libclang-bindings; echo source root reset to $sourceRoot";
   libraryHaskellDepends = [
     base bytestring data-default directory exceptions filepath process
     template-haskell text transformers unliftio-core
