cd5b44122810793917198ca834d45ed47b4d087d
Joris Dral <joris@well-typed.com>
2026-09-02 10:29:41 +0200

    `UniqueExpansions`: parse either as object-like or function-like, not both
    In `HsBindgen.Clang.Macros.UniqueExpansion.Parse`, we would previously try to
    parse a macro definition first as a function-like macro, and if that failed, as
    an object-like macro. The way the parser was structured, we would sometimes
    parse function-like macros as object-like, even though we already knew for sure
    that it was function-like. The parser is now updated to not have such fallback
    behaviour: we determine ASAP whether the macro definition is function-like or
    object-like, and chooses different parser paths based on that.

--- hs-bindgen/src-internal/HsBindgen/Clang/Macros/UniqueExpansion/Parse.hs
+++ hs-bindgen/src-internal/HsBindgen/Clang/Macros/UniqueExpansion/Parse.hs
@@ -10,8 +10,8 @@ module HsBindgen.Clang.Macros.UniqueExpansion.Parse (
 
 import Control.Monad (guard, unless)
 import Data.Maybe (catMaybes)
-import Text.Parsec (anyToken, choice, lookAhead, many, manyTill, parserFail,
-                    sepBy, try)
+import Text.Parsec (anyToken, choice, lookAhead, many, manyTill, option, sepBy,
+                    try, unexpected)
 
 import Clang.Enum.Simple (fromSimpleEnum)
 import Clang.HighLevel.Types (MultiLoc (multiLocExpansion),
@@ -35,20 +35,31 @@ import HsBindgen.Clang.Macros.UniqueExpansion.Types (Definition (..),
 parseDefinition :: Parser Definition
 parseDefinition = do
     (macroLocRange, macroName) <- parseLocName
-    let
-        functionLike :: Parser Definition
-        functionLike = do
-          noWhitespace macroLocRange
-          paramNames <- parseParams
-          macroBody <- parseBody paramNames
-          pure $ Definition { name = macroName, params = paramNames, body = macroBody }
+    isFunction <- isFunctionLike macroLocRange
+    params <-
+      if isFunction then parseParams else pure []
+    macroBody <- parseBody params
+    pure Definition {
+        name = macroName
+      , params = params
+      , body = macroBody
+      }
 
-        objectLike :: Parser Definition
-        objectLike = do
-          let paramNames = []
-          macroBody <- parseBody paramNames
-          pure $ Definition { name = macroName, params = paramNames, body = macroBody }
-    choice [try functionLike, objectLike]
+-- | Check if a macro definition is function-like. If not, then the definition
+-- is object-like.
+--
+-- A macro definition is function-like if the macro definition's name is
+-- followed immediately by a ( character without any whitespace in between. See
+-- 'lparen'.
+--
+-- @isFunctionLike@ does not consume input.
+isFunctionLike ::
+     -- | Source location of the macro definition's name
+     Range MultiLoc
+  -> Parser Bool
+isFunctionLike prevRange =
+    --  is function-like if the macro name
+    lookAhead (option False (True <$ try (lparen prevRange)))
 
 parseBody :: [Name] -> Parser [Var]
 parseBody params = catMaybes <$> many (choice [parseVar, parseNonVar])
@@ -107,30 +118,27 @@ parseArgs = fmap concat $ do
       (try (punctuation ")"))
 
 {-------------------------------------------------------------------------------
-  Whitespace
+  Punctuation
 -------------------------------------------------------------------------------}
 
--- | Check that there is no whitespace between the previous token and the current
--- token
+-- | Parse a ( character not immediately preceded by white space
 --
--- Function-like macros are only function-like if there is /no/ whitespace
--- between the macro name and the opening parenthesis of the parameter list.
+-- @lparen@ consumes input when it fails. Combine with @try@ if this is
+-- undesirable.
+--
+-- NOTE: @lparen@ is defined in the C reference
 --
 -- We used to not check whitespace, which was the source of a bug. See issue
 -- #1903: <https://github.com/well-typed/hs-bindgen/issues/1903>
-noWhitespace ::
-     -- | Source range for the previous token
-     Range MultiLoc
-  -> Parser ()
-noWhitespace prevRange = lookAhead $ do
+lparen :: Range MultiLoc -> Parser ()
+lparen prevRange = do
     tok <- anyToken
     let prev    = prevRange.rangeEnd.multiLocExpansion
         current = tok.tokenExtent.rangeStart.multiLocExpansion
         p       = prev.singleLocPath   == current.singleLocPath &&
                   prev.singleLocLine   == current.singleLocLine &&
                   prev.singleLocColumn == current.singleLocColumn
-    unless p $
-      parserFail "unexpected whitespace"
+    unless p $ unexpected "whitespace before lparen"
 
 {-------------------------------------------------------------------------------
   Identifiers
